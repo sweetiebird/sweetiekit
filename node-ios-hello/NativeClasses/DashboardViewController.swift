efc6b42266fbfbc4505c7b870cce6eca850e70b2
Emily Kolar <emkolar@gmail.com>
2019-04-24 23:53:31 -0500

    update ignore; ignore modules

---
+++ node-ios-hello/NativeClasses/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  node-ios-hello
+//
+//  Created by Emily Kolar on 4/23/19.
+//  Copyright © 2019 sweetiebird. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
