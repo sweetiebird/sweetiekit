11b992a86278c749ab1447e0c341319fbe43559c
Emily Kolar <emkolar@gmail.com>
2019-05-13 18:31:25 -0500

    update demos

--- Scripts/examples/UIButton.js
+++ Scripts/examples/UIButton.js
@@ -4,12 +4,16 @@ const {
   UIButton,
 } = SweetieKit;
 
-async function make() {
-  const button = await UIButton.alloc('👋 Hello Button', 0, 0, 200, 50, () => {
+async function make(demoVC) {
+  const w = demoVC.view.frame.width;
+  const button = await UIButton.alloc('👋 Hello Button', 12, 80, w - 24, 50, () => {
     console.log('button pressed');
   });
   button.backgroundColor = { red: 87/255, green: 174/255, blue: 176/255 };
   button.layer.cornerRadius = 4;
+  button.layer.shadowRadius = 12;
+  button.layer.shadowColor = { red: 87/255, green: 174/255, blue: 176/255 };
+  button.layer.shadowOffset = { width: 0, height: 12 };
   return button;
 }
 
