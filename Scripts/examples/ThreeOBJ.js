87b4c214c708e4dd0d45049375a4a739467c5091
Emily Kolar <emkolar@gmail.com>
2019-05-13 20:47:07 -0500

    0.2 size box

--- Scripts/examples/ThreeOBJ.js
+++ Scripts/examples/ThreeOBJ.js
@@ -5,7 +5,7 @@ require('../vendor/three/OBJExporter');
 
 async function make(demoVC) {
   const exporter = new THREE.OBJExporter();
-  const geometry = new THREE.BoxBufferGeometry(0.1, 1, 1);
+  const geometry = new THREE.BoxBufferGeometry(0.2, 0.2, 0.2);
   const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
   const mesh = new THREE.Mesh(geometry, material);
   const output = exporter.parse(mesh);
