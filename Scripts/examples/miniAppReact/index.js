eb3d299fb75f08853dcace6d13c5ca79994686a3
Emily Kolar <emkolar@gmail.com>
2019-05-21 03:31:36 -0500

    theme app

--- Scripts/examples/miniAppReact/index.js
+++ Scripts/examples/miniAppReact/index.js
@@ -5,7 +5,8 @@ SweetieKitDom();
 React = window.React = require('react');
 ReactDOM = window.ReactDOM = require('react-dom');
 
-const { UIControlState } = require('../enums');
+const { UIControlState, UIControlEvents } = require('../enums');
+const colors = require('../colors');
 
 const {
   UIButton,
@@ -16,48 +17,78 @@ div.setAttribute('id', 'app');
 document.body.appendChild(div);
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
+  componentDidMount() {
+    const { superview, onClick } = this.props;
+    const w = superview.frame.width;
+    this.ui = new UIButton({ x: 12, y: 80, width: w - 24, height: 50 });
+    this.ui.backgroundColor = { red: 87/255, green: 174/255, blue: 176/255 };
+    this.ui.layer.cornerRadius = 4;
+    this.ui.layer.shadowRadius = 12;
+    this.ui.layer.shadowColor = { red: 87/255, green: 174/255, blue: 176/255 };
+    this.ui.layer.shadowOffset = { width: 0, height: 12 };
+    this.ui.setTitleForState('Toggle Me', UIControlState.normal);
+    this.ui.addTarget(onClick, UIControlEvents.touchUpInside);
+    superview.addSubview(this.ui);
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { isToggleOn } = this.state;
-  //   const { isToggleOn: wasToggleOn } = prevState;
-  //   if (isToggleOn !== wasToggleOn) {
-  //     this.button.setTitleForState();
-  //   }
-  // }
-
-  async componentDidMount() {
-    const { vc } = this.props;
-    const w = vc.view.frame.width;
-    this.button = await UIButton.alloc('Hello Button', 12, 80, w - 24, 50, this.handleClick);
-    this.button.backgroundColor = { red: 87/255, green: 174/255, blue: 176/255 };
-    this.button.layer.cornerRadius = 4;
-    this.button.layer.shadowRadius = 12;
-    this.button.layer.shadowColor = { red: 87/255, green: 174/255, blue: 176/255 };
-    this.button.layer.shadowOffset = { width: 0, height: 12 };
-    vc.view.addSubview(this.button);
+  componentDidUpdate(prevProps) {
+    const { isDarkTheme } = this.props;
+    const { isDarkTheme: wasDarkTheme } = prevProps;
+    if (isDarkTheme !== wasDarkTheme) {
+      if (isDarkTheme) {
+        this.ui.titleLabel.textColor = colors.white;
+        this.ui.backgroundColor = colors.fitbodMedGrey;
+      } else {
+        this.ui.titleLabel.textColor = colors.fitbodDarkGrey;
+        this.ui.backgroundColor = colors.fitbodPink;
+      }
+    }
   }
 
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-    }), () => {
-      console.log(this.state.isToggleOn);
-    });
+  componentWillUnmount() {
+    this.ui.removeFromSuperview();
   }
 
   render() {
-    if (this.button) {
-      this.button.setTitleForState(this.state.isToggleOn ? 'ON' : 'OFF', UIControlState.normal);
-    }
+    return React.createElement('button', {}, );
+  }
+}
 
-    return React.createElement("button", {
-      onClick: this.handleClick,
+class FullScreenView extends React.Component {
+  componentWillMount() {
+    const { superview, isDarkTheme } = this.props;
+    this.ui = new UIView({ x: 0, y: 0, width: superview.frame.width, height: superview.frame.height });
+    if (isDarkTheme) {
+      this.ui.backgroundColor = colors.fitbodDarkGrey;
+    } else {
+      this.ui.backgroundColor = colors.white;
+    }
+    superview.addSubview(this.ui);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { isDarkTheme } = this.props;
+    const { isDarkTheme: wasDarkTheme } = prevProps;
+    if (isDarkTheme !== wasDarkTheme) {
+      if (isDarkTheme) {
+        this.ui.backgroundColor = colors.fitbodMedGrey;
+      } else {
+        this.ui.backgroundColor = colors.white;
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.ui.removeFromSuperview();
+  }
+
+  render() {
+    const { children, isDarkTheme, onButtonClick } = this.props;
+
+    return React.createElement(Button, {
+      superview: this.ui,
+      isDarkTheme,
+      onClick: onButtonClick,
     });
   }
 }
@@ -65,6 +96,10 @@ class Button extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isDarkTheme: false,
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   componentDidMount() {
@@ -72,15 +107,19 @@ class App extends React.Component {
     nav.pushViewController(vc);
   }
 
-  // handleClick() {
-  //
-  // }
+  handleButtonClick() {
+    const { isDarkTheme } = this.state;
+    this.setState({ isDarkTheme: !isDarkTheme });
+  }
 
   render() {
     const { vc } = this.props;
+    const { isDarkTheme } = this.state;
 
-    return React.createElement(Button, {
-      vc,
+    return React.createElement(FullScreenView, {
+      isDarkTheme,
+      onButtonClick: this.handleButtonClick,
+      superview: vc.view,
     });
   }
 }
