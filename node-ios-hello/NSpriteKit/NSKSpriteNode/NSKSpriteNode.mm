8f000b9fa8ccadd073d1159c966748ccae4028ab
Shawn Presser <shawnpresser@gmail.com>
2019-05-29 10:02:57 -0500

    Expose properties on SKSpriteNode

--- node-ios-hello/NSpriteKit/NSKSpriteNode/NSKSpriteNode.mm
+++ node-ios-hello/NSpriteKit/NSKSpriteNode/NSKSpriteNode.mm
@@ -30,10 +30,23 @@ std::pair<Local<Object>, Local<FunctionTemplate>> NSKSpriteNode::Initialize(Isol
 
   // prototype
   Local<ObjectTemplate> proto = ctor->PrototypeTemplate();
-  JS_ASSIGN_PROP(proto, size);
+//  JS_ASSIGN_PROP(proto, size);
+//  JS_ASSIGN_PROP(proto, colorBlendFactor);
+//  JS_ASSIGN_PROP(proto, color);
+  JS_ASSIGN_METHOD(proto, scaleToSize);
+  JS_ASSIGN_PROP(proto, texture);
+  JS_ASSIGN_PROP(proto, normalTexture);
+  JS_ASSIGN_PROP(proto, lightingBitMask);
+  JS_ASSIGN_PROP(proto, shadowCastBitMask);
+  JS_ASSIGN_PROP(proto, shadowedBitMask);
+  JS_ASSIGN_PROP(proto, centerRect);
   JS_ASSIGN_PROP(proto, colorBlendFactor);
   JS_ASSIGN_PROP(proto, color);
-  Nan::SetMethod(proto, "scaleToSize", scaleToSize);
+  JS_ASSIGN_PROP(proto, blendMode);
+  JS_ASSIGN_PROP(proto, anchorPoint);
+  JS_ASSIGN_PROP(proto, size);
+  JS_ASSIGN_PROP(proto, shader);
+  JS_ASSIGN_PROP(proto, attributeValues);
 
   // ctor
   Local<Function> ctorFn = Nan::GetFunction(ctor).ToLocalChecked();
@@ -107,7 +120,7 @@ NAN_METHOD(NSKSpriteNode::New) {
 
 NSKSpriteNode::NSKSpriteNode () {}
 NSKSpriteNode::~NSKSpriteNode () {}
-
+/*
 NAN_GETTER(NSKSpriteNode::sizeGetter) {
   Nan::HandleScope scope;
 
@@ -180,19 +193,6 @@ NAN_SETTER(NSKSpriteNode::colorSetter) {
   }
 }
 
-NAN_METHOD(NSKSpriteNode::scaleToSize) {
-  Nan::HandleScope scope;
-
-  JS_UNWRAP(SKSpriteNode, node);
-
-  @autoreleasepool {
-    double width = TO_DOUBLE(JS_OBJ(info[0])->Get(JS_STR("width")));
-    double height = TO_DOUBLE(JS_OBJ(info[0])->Get(JS_STR("height")));
-    CGSize size = CGSizeMake(width, height);
-    [node scaleToSize:size];
-  }
-}
-
 NAN_GETTER(NSKSpriteNode::anchorPointGetter) {
   Nan::HandleScope scope;
 
@@ -224,3 +224,243 @@ NAN_SETTER(NSKSpriteNode::anchorPointSetter) {
     [node setAnchorPoint:CGPointMake(x, y)];
   }
 }
+*/
+
+NAN_METHOD(NSKSpriteNode::scaleToSize) {
+  JS_UNWRAP(SKSpriteNode, node);
+  @autoreleasepool
+  {
+    [node scaleToSize:to_value_CGSize(info[0])];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::textureGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_SKTexture([self texture]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::textureSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setTexture: to_value_SKTexture(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::normalTextureGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_SKTexture([self normalTexture]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::normalTextureSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setNormalTexture: to_value_SKTexture(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::lightingBitMaskGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(JS_UINT([self lightingBitMask]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::lightingBitMaskSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setLightingBitMask: TO_UINT32(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::shadowCastBitMaskGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(JS_UINT([self shadowCastBitMask]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::shadowCastBitMaskSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setShadowCastBitMask: TO_UINT32(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::shadowedBitMaskGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(JS_UINT([self shadowedBitMask]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::shadowedBitMaskSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setShadowedBitMask: TO_UINT32(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::centerRectGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGRect([self centerRect]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::centerRectSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setCenterRect: to_value_CGRect(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::colorBlendFactorGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(JS_FLOAT([self colorBlendFactor]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::colorBlendFactorSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setColorBlendFactor: TO_FLOAT(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::colorGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_SKColor([self color]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::colorSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setColor: to_value_SKColor(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::blendModeGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_SKBlendMode([self blendMode]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::blendModeSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setBlendMode: to_value_SKBlendMode(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::anchorPointGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGPoint([self anchorPoint]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::anchorPointSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setAnchorPoint: to_value_CGPoint(value)];
+  }
+}
+
+NAN_GETTER(NSKSpriteNode::sizeGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGSize([self size]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::sizeSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setSize: to_value_CGSize(value)];
+  }
+}
+
+#define js_value_SKShader(x) js_value_wrapper_unknown(x, SKShader)
+#define to_value_SKShader(x) to_value_wrapper_unknown(x, SKShader)
+
+NAN_GETTER(NSKSpriteNode::shaderGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_SKShader([self shader]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::shaderSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    [self setShader: to_value_SKShader(value)];
+  }
+}
+
+#define js_value_SKAttributeValue(x) js_value_wrapper_unknown(x, SKAttributeValue)
+#define to_value_SKAttributeValue(x) to_value_wrapper_unknown(x, SKAttributeValue)
+
+NAN_GETTER(NSKSpriteNode::attributeValuesGetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    Nan::ThrowError("Not implemented");
+//    JS_SET_RETURN(js_value_NSDictionary<NSString*, SKAttributeValue* >([self attributeValues]));
+    return;
+  }
+}
+
+NAN_SETTER(NSKSpriteNode::attributeValuesSetter) {
+  JS_UNWRAP(SKSpriteNode, self);
+  @autoreleasepool
+  {
+    Nan::ThrowError("Not implemented");
+//    [self setAttributeValues: to_value_NSDictionary<NSString*, SKAttributeValue* >(value)];
+  }
+}
+
+
