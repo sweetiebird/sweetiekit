9fc62c2e57540591d9249ec3d5f41d06557549cd
Shawn Presser <shawnpresser@gmail.com>
2019-06-02 00:32:01 -0500

    Add comment

--- node-ios-hello/NUIKit/NUIViewController/NUIViewController.mm
+++ node-ios-hello/NUIKit/NUIViewController/NUIViewController.mm
@@ -70,7 +70,7 @@ NAN_METHOD(NUIViewController::New) {
     if (info[0]->IsExternal()) {
       self = (__bridge UIViewController *)(info[0].As<External>()->Value());
     } else if (info.Length() <= 0) {
-      self = [UIViewController alloc];
+      self = [UIViewController alloc]; // TODO: init causes layout issues
     }
     if (self) {
       NUIViewController *wrapper = new NUIViewController();
