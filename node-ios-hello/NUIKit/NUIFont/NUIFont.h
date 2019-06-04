2f585c501f2b90251107e23e7b349f02f11d38a3
Shawn Presser <shawnpresser@gmail.com>
2019-06-04 07:20:08 -0500

    Expose the rest of UIFont

--- node-ios-hello/NUIKit/NUIFont/NUIFont.h
+++ node-ios-hello/NUIKit/NUIFont/NUIFont.h
@@ -15,10 +15,41 @@
 #define to_value_UIFont(x) to_value_wrapper(x, UIFont)
 #define is_value_UIFont(x) is_value_wrapper(x, UIFont)
 
+// UIKit constants
+
+#define js_value_UIFontTextStyle js_value_NSString
+#define to_value_UIFontTextStyle to_value_NSString
+#define is_value_UIFontTextStyle is_value_NSString
+
 JS_WRAP_CLASS(UIFont, NSObject);
+  JS_METHOD(fontWithSize);
+  JS_PROP_READONLY(familyName);
+  JS_PROP_READONLY(fontName);
+  JS_PROP_READONLY(pointSize);
+  JS_PROP_READONLY(ascender);
+  JS_PROP_READONLY(descender);
+  JS_PROP_READONLY(capHeight);
+  JS_PROP_READONLY(xHeight);
+  JS_PROP_READONLY(lineHeight);
+  JS_PROP_READONLY(leading);
+  JS_PROP_READONLY(fontDescriptor);
+  // static properties
+  JS_PROP_READONLY(familyNames);
+  // static methods
   JS_METHOD(systemFont);
   JS_METHOD(boldSystemFont);
   JS_METHOD(italicSystemFont);
+  JS_METHOD(preferredFontForTextStyle);
+  JS_METHOD(preferredFontForTextStyleCompatibleWithTraitCollection);
+  JS_METHOD(fontWithNameSize);
+  JS_METHOD(fontNamesForFamilyName);
+  JS_METHOD(systemFontOfSize);
+  JS_METHOD(boldSystemFontOfSize);
+  JS_METHOD(italicSystemFontOfSize);
+  JS_METHOD(systemFontOfSizeWeight);
+  JS_METHOD(monospacedDigitSystemFontOfSizeWeight);
+  JS_METHOD(fontWithDescriptorSize);
+
 JS_WRAP_CLASS_END(UIFont);
 
 #endif /* NUIFont_h */
--- node-ios-hello/NUIKit/NUIFont/NUIFont.mm
+++ node-ios-hello/NUIKit/NUIFont/NUIFont.mm
@@ -11,11 +11,33 @@ NUIFont::~NUIFont() {}
 
 JS_INIT_CLASS(UIFont, NSObject);
   // instance members (proto)
+  JS_ASSIGN_METHOD(proto, fontWithSize);
+  JS_ASSIGN_PROP_READONLY(proto, familyName);
+  JS_ASSIGN_PROP_READONLY(proto, fontName);
+  JS_ASSIGN_PROP_READONLY(proto, pointSize);
+  JS_ASSIGN_PROP_READONLY(proto, ascender);
+  JS_ASSIGN_PROP_READONLY(proto, descender);
+  JS_ASSIGN_PROP_READONLY(proto, capHeight);
+  JS_ASSIGN_PROP_READONLY(proto, xHeight);
+  JS_ASSIGN_PROP_READONLY(proto, lineHeight);
+  JS_ASSIGN_PROP_READONLY(proto, leading);
+  JS_ASSIGN_PROP_READONLY(proto, fontDescriptor);
   // static members (ctor)
   JS_INIT_CTOR(UIFont, NSObject);
+  JS_ASSIGN_PROP_READONLY(JS_OBJ(ctor), familyNames);
   JS_ASSIGN_METHOD(ctor, systemFont);
   JS_ASSIGN_METHOD(ctor, boldSystemFont);
   JS_ASSIGN_METHOD(ctor, italicSystemFont);
+  JS_ASSIGN_METHOD(ctor, preferredFontForTextStyle);
+  JS_ASSIGN_METHOD(ctor, preferredFontForTextStyleCompatibleWithTraitCollection);
+  JS_ASSIGN_METHOD(ctor, fontWithNameSize);
+  JS_ASSIGN_METHOD(ctor, fontNamesForFamilyName);
+  JS_ASSIGN_METHOD(ctor, systemFontOfSize);
+  JS_ASSIGN_METHOD(ctor, boldSystemFontOfSize);
+  JS_ASSIGN_METHOD(ctor, italicSystemFontOfSize);
+  JS_ASSIGN_METHOD(ctor, systemFontOfSizeWeight);
+  JS_ASSIGN_METHOD(ctor, monospacedDigitSystemFontOfSizeWeight);
+  JS_ASSIGN_METHOD(ctor, fontWithDescriptorSize);
 JS_INIT_CLASS_END(UIFont, NSObject);
 
 NAN_METHOD(NUIFont::New) {
@@ -95,3 +117,204 @@ NAN_METHOD(NUIFont::italicSystemFont) {
 
   JS_SET_RETURN(obj);
 }
+
+NAN_METHOD(NUIFont::preferredFontForTextStyle) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(UIFontTextStyle, style);
+    JS_SET_RETURN(js_value_UIFont([UIFont preferredFontForTextStyle: style]));
+  }
+}
+
+#define js_value_UITraitCollection(x) js_value_wrapper_unknown(x, UITraitCollection)
+#define to_value_UITraitCollection(x) to_value_wrapper_unknown(x, UITraitCollection)
+#define is_value_UITraitCollection(x) is_value_wrapper_unknown(x, UITraitCollection)
+
+NAN_METHOD(NUIFont::preferredFontForTextStyleCompatibleWithTraitCollection) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(UIFontTextStyle, style);
+    declare_pointer(UITraitCollection, traitCollection);
+    JS_SET_RETURN(js_value_UIFont([UIFont preferredFontForTextStyle: style compatibleWithTraitCollection: traitCollection]));
+  }
+}
+
+NAN_METHOD(NUIFont::fontWithNameSize) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_pointer(NSString, fontName);
+    declare_value(CGFloat, fontSize);
+    JS_SET_RETURN(js_value_UIFont([UIFont fontWithName: fontName size: fontSize]));
+  }
+}
+
+NAN_METHOD(NUIFont::fontNamesForFamilyName) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_pointer(NSString, familyName);
+    JS_SET_RETURN(js_value_NSArray<NSString*>([UIFont fontNamesForFamilyName: familyName]));
+  }
+}
+
+NAN_METHOD(NUIFont::systemFontOfSize) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    JS_SET_RETURN(js_value_UIFont([UIFont systemFontOfSize: fontSize]));
+  }
+}
+
+NAN_METHOD(NUIFont::boldSystemFontOfSize) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    JS_SET_RETURN(js_value_UIFont([UIFont boldSystemFontOfSize: fontSize]));
+  }
+}
+
+NAN_METHOD(NUIFont::italicSystemFontOfSize) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    JS_SET_RETURN(js_value_UIFont([UIFont italicSystemFontOfSize: fontSize]));
+  }
+}
+
+NAN_METHOD(NUIFont::systemFontOfSizeWeight) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    declare_value(CGFloat, weight);
+    JS_SET_RETURN(js_value_UIFont([UIFont systemFontOfSize: fontSize weight: weight]));
+  }
+}
+
+NAN_METHOD(NUIFont::monospacedDigitSystemFontOfSizeWeight) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    declare_value(CGFloat, weight);
+    JS_SET_RETURN(js_value_UIFont([UIFont monospacedDigitSystemFontOfSize: fontSize weight: weight]));
+  }
+}
+
+#define js_value_UIFontDescriptor(x) js_value_wrapper_unknown(x, UIFontDescriptor)
+#define to_value_UIFontDescriptor(x) to_value_wrapper_unknown(x, UIFontDescriptor)
+#define is_value_UIFontDescriptor(x) is_value_wrapper_unknown(x, UIFontDescriptor)
+
+NAN_METHOD(NUIFont::fontWithDescriptorSize) {
+  @autoreleasepool
+  {
+    declare_args();
+    declare_pointer(UIFontDescriptor, descriptor);
+    declare_value(CGFloat, pointSize);
+    JS_SET_RETURN(js_value_UIFont([UIFont fontWithDescriptor: descriptor size: pointSize]));
+  }
+}
+
+NAN_METHOD(NUIFont::fontWithSize) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    declare_args();
+    declare_value(CGFloat, fontSize);
+    JS_SET_RETURN(js_value_UIFont([self fontWithSize: fontSize]));
+  }
+}
+
+NAN_GETTER(NUIFont::familyNamesGetter) {
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_NSArray<NSString*>([UIFont familyNames]));
+  }
+}
+
+NAN_GETTER(NUIFont::familyNameGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_NSString([self familyName]));
+  }
+}
+
+NAN_GETTER(NUIFont::fontNameGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_NSString([self fontName]));
+  }
+}
+
+NAN_GETTER(NUIFont::pointSizeGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self pointSize]));
+  }
+}
+
+NAN_GETTER(NUIFont::ascenderGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self ascender]));
+  }
+}
+
+NAN_GETTER(NUIFont::descenderGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self descender]));
+  }
+}
+
+NAN_GETTER(NUIFont::capHeightGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self capHeight]));
+  }
+}
+
+NAN_GETTER(NUIFont::xHeightGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self xHeight]));
+  }
+}
+
+NAN_GETTER(NUIFont::lineHeightGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self lineHeight]));
+  }
+}
+
+NAN_GETTER(NUIFont::leadingGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_CGFloat([self leading]));
+  }
+}
+
+NAN_GETTER(NUIFont::fontDescriptorGetter) {
+  JS_UNWRAP(UIFont, self);
+  @autoreleasepool
+  {
+    JS_SET_RETURN(js_value_UIFontDescriptor([self fontDescriptor]));
+  }
+}
+
