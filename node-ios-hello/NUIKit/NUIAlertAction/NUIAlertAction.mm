f068e3e08859edd290f137d59220baa72ada6f52
Shawn Presser <shawnpresser@gmail.com>
2019-07-04 09:52:57 -0500

    Re-expose UIAlertController, UIAlertAction

--- node-ios-hello/NUIKit/NUIAlertAction/NUIAlertAction.mm
+++ node-ios-hello/NUIKit/NUIAlertAction/NUIAlertAction.mm
@@ -6,45 +6,96 @@
 //
 #include "NUIAlertAction.h"
 
+#define instancetype UIAlertAction
+#define js_value_instancetype js_value_UIAlertAction
+
 NUIAlertAction::NUIAlertAction() {}
 NUIAlertAction::~NUIAlertAction() {}
 
 JS_INIT_CLASS(UIAlertAction, NSObject);
+  JS_ASSIGN_STATIC_METHOD(actionWithTitleStyleHandler);
+  JS_ASSIGN_PROTO_PROP_READONLY(title);
+  JS_ASSIGN_PROTO_PROP_READONLY(style);
+  JS_ASSIGN_PROTO_PROP(isEnabled);
+
   // instance members (proto)
   // static members (ctor)
   JS_INIT_CTOR(UIAlertAction, NSObject);
+  // constants (exports)
+
+  //typedef NS_ENUM(NSInteger, UIAlertActionStyle) {
+    JS_ASSIGN_ENUM(UIAlertActionStyleDefault, NSInteger); //  = 0,
+    JS_ASSIGN_ENUM(UIAlertActionStyleCancel, NSInteger);
+    JS_ASSIGN_ENUM(UIAlertActionStyleDestructive, NSInteger);
+  //} NS_ENUM_AVAILABLE_IOS(8_0);
+
 JS_INIT_CLASS_END(UIAlertAction, NSObject);
 
 NAN_METHOD(NUIAlertAction::New) {
   JS_RECONSTRUCT(UIAlertAction);
-
-  Local<Object> obj = info.This();
-
-  NUIAlertAction *action = new NUIAlertAction();
-
-  if (info[0]->IsExternal()) {
-    action->SetNSObject((__bridge UIAlertAction *)(info[0].As<External>()->Value()));
-  } else {
-    std::string str;
-    if (info[0]->IsString()) {
-      Nan::Utf8String utf8Value(Local<String>::Cast(info[0]));
-      str = *utf8Value;
-    } else {
-      Nan::ThrowError("invalid argument");
+  @autoreleasepool {
+    UIAlertAction* self = nullptr;
+    
+    if (info[0]->IsExternal()) {
+      self = (__bridge UIAlertAction *)(info[0].As<External>()->Value());
+    } else if (info.Length() <= 0) {
+      self = [[UIAlertAction alloc] init];
     }
-    @autoreleasepool {
-      NSString *title = [NSString stringWithUTF8String:str.c_str()];
-      UIAlertAction* ui = action->SetNSObject([UIAlertAction actionWithTitle:title style:UIAlertActionStyleDefault handler: ^ (UIAlertAction * _Nonnull act) {
-        dispatch_main(^{
-          Local<Value> actionObj = sweetiekit::GetWrapperFor(act, NUIAlertAction::type);
-          JS_GET_FUNCTION(fn, act, @"sweetiekit_UIAlertAction_callback");
-          fn("NUITableViewManager::New", actionObj);
-        });
-      }]);
-      JS_ATTACH_FUNCTION(info[1], ui, @"sweetiekit_UIAlertAction_callback");
+    if (self) {
+      NUIAlertAction *wrapper = new NUIAlertAction();
+      wrapper->SetNSObject(self);
+      Local<Object> obj(info.This());
+      wrapper->Wrap(obj);
+      JS_SET_RETURN(obj);
+    } else {
+      Nan::ThrowError("UIAlertAction::New: invalid arguments");
     }
   }
-  action->Wrap(obj);
+}
 
-  JS_SET_RETURN(obj);
+NAN_METHOD(NUIAlertAction::actionWithTitleStyleHandler) {
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(NSString, title);
+    declare_value(UIAlertActionStyle, style);
+    declare_callback(handler);
+    JS_SET_RETURN(js_value_instancetype([UIAlertAction actionWithTitle: title style: style handler:^(UIAlertAction * _Nonnull action) {
+      dispatch_main(^{
+        if (handler) {
+          [handler jsFunction]->Call("NUIAlertAction::actionWithTitleStyleHandler",
+            js_value_UIAlertAction(action));
+        }
+      });
+    }]));
+  }
+}
+
+NAN_GETTER(NUIAlertAction::titleGetter) {
+  JS_UNWRAP(UIAlertAction, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSString([self title]));
+  }
+}
+
+NAN_GETTER(NUIAlertAction::styleGetter) {
+  JS_UNWRAP(UIAlertAction, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UIAlertActionStyle([self style]));
+  }
+}
+
+NAN_GETTER(NUIAlertAction::isEnabledGetter) {
+  JS_UNWRAP(UIAlertAction, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_BOOL([self isEnabled]));
+  }
+}
+
+NAN_SETTER(NUIAlertAction::isEnabledSetter) {
+  JS_UNWRAP(UIAlertAction, self);
+  declare_autoreleasepool {
+    declare_setter();
+    declare_value(BOOL, input);
+    [self setEnabled: input];
+  }
 }
--- node-ios-hello/NUIKit/NUIAlertController/NUIAlertController.mm
+++ node-ios-hello/NUIKit/NUIAlertController/NUIAlertController.mm
@@ -6,63 +6,161 @@
 //
 #include "NUIAlertController.h"
 
+#define instancetype UIAlertController
+#define js_value_instancetype js_value_UIAlertController
+
 NUIAlertController::NUIAlertController() {}
 NUIAlertController::~NUIAlertController() {}
 
 JS_INIT_CLASS(UIAlertController, UIViewController);
+  JS_ASSIGN_STATIC_METHOD(alertControllerWithTitleMessagePreferredStyle);
+  JS_ASSIGN_PROTO_METHOD(addAction);
+  JS_ASSIGN_PROTO_METHOD(addTextFieldWithConfigurationHandler);
+  JS_ASSIGN_PROTO_PROP_READONLY(actions);
+  JS_ASSIGN_PROTO_PROP(preferredAction);
+  JS_ASSIGN_PROTO_PROP_READONLY(textFields);
+  JS_ASSIGN_PROTO_PROP(title);
+  JS_ASSIGN_PROTO_PROP(message);
+  JS_ASSIGN_PROTO_PROP_READONLY(preferredStyle);
+
   // instance members (proto)
-  JS_SET_METHOD(proto, "addAction", AddAction);
   // static members (ctor)
   JS_INIT_CTOR(UIAlertController, UIViewController);
+  // constants (exports)
+
+  //typedef NS_ENUM(NSInteger, UIAlertControllerStyle) {
+    JS_ASSIGN_ENUM(UIAlertControllerStyleActionSheet, NSInteger); //  = 0,
+    JS_ASSIGN_ENUM(UIAlertControllerStyleAlert, NSInteger);
+  //} NS_ENUM_AVAILABLE_IOS(8_0);
+
 JS_INIT_CLASS_END(UIAlertController, UIViewController);
 
 NAN_METHOD(NUIAlertController::New) {
   JS_RECONSTRUCT(UIAlertController);
-
-  Local<Object> ctrlObj = info.This();
-
-  NUIAlertController *ctrl = new NUIAlertController();
-
-  if (info[0]->IsExternal()) {
-    ctrl->SetNSObject((__bridge UIAlertController *)(info[0].As<External>()->Value()));
-  } else {
-    std::string title;
-    std::string message;
-    if (info[0]->IsString()) {
-      Nan::Utf8String utf8Value(Local<String>::Cast(info[0]));
-      title = *utf8Value;
-    } else {
-      Nan::ThrowError("invalid argument");
+  @autoreleasepool {
+    UIAlertController* self = nullptr;
+    
+    if (info[0]->IsExternal()) {
+      self = (__bridge UIAlertController *)(info[0].As<External>()->Value());
+    } else if (info.Length() <= 0) {
+      self = [[UIAlertController alloc] init];
     }
-    if (info[1]->IsString()) {
-      Nan::Utf8String utf8Value(Local<String>::Cast(info[1]));
-      message = *utf8Value;
+    if (self) {
+      NUIAlertController *wrapper = new NUIAlertController();
+      wrapper->SetNSObject(self);
+      Local<Object> obj(info.This());
+      wrapper->Wrap(obj);
+      JS_SET_RETURN(obj);
     } else {
-      Nan::ThrowError("invalid argument");
-    }
-
-    @autoreleasepool {
-      NSString *titleStr = [NSString stringWithUTF8String:title.c_str()];
-      NSString *messageStr = [NSString stringWithUTF8String:message.c_str()];
-      ctrl->SetNSObject([UIAlertController alertControllerWithTitle:titleStr message:messageStr preferredStyle:UIAlertControllerStyleAlert]);
+      Nan::ThrowError("UIAlertController::New: invalid arguments");
     }
   }
-  ctrl->Wrap(ctrlObj);
+}
 
-  info.GetReturnValue().Set(ctrlObj);
+NAN_METHOD(NUIAlertController::alertControllerWithTitleMessagePreferredStyle) {
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(NSString, title);
+    declare_nullable_pointer(NSString, message);
+    declare_value(UIAlertControllerStyle, preferredStyle);
+    JS_SET_RETURN(js_value_instancetype([UIAlertController alertControllerWithTitle: title message: message preferredStyle: preferredStyle]));
+  }
 }
 
 #include "NUIAlertAction.h"
 
-NAN_METHOD(NUIAlertController::AddAction) {
-  Nan::HandleScope scope;
-  
-  JS_UNWRAP(UIAlertController, ui);
-
-  NUIAlertAction *actionObj = ObjectWrap::Unwrap<NUIAlertAction>(Local<Object>::Cast(info[0]));
-
-  @autoreleasepool {
-    [ui addAction:actionObj->As<UIAlertAction>()];
+NAN_METHOD(NUIAlertController::addAction) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_pointer(UIAlertAction, action);
+    [self addAction: action];
   }
 }
 
+#include "NUITextField.h"
+
+NAN_METHOD(NUIAlertController::addTextFieldWithConfigurationHandler) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_callback(configurationHandler);
+    [self addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+      dispatch_main(^{
+        if (configurationHandler) {
+          [configurationHandler jsFunction]->Call("NUIAlertController::addTextFieldWithConfigurationHandler",
+            js_value_UITextField(textField));
+        }
+      });
+    }];
+  }
+}
+
+NAN_GETTER(NUIAlertController::actionsGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSArray<UIAlertAction*>([self actions]));
+  }
+}
+
+NAN_GETTER(NUIAlertController::preferredActionGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UIAlertAction([self preferredAction]));
+  }
+}
+
+NAN_SETTER(NUIAlertController::preferredActionSetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    declare_setter();
+    declare_pointer(UIAlertAction, input);
+    [self setPreferredAction: input];
+  }
+}
+
+NAN_GETTER(NUIAlertController::textFieldsGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSArray<UITextField*>([self textFields]));
+  }
+}
+
+NAN_GETTER(NUIAlertController::titleGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSString([self title]));
+  }
+}
+
+NAN_SETTER(NUIAlertController::titleSetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    declare_setter();
+    declare_pointer(NSString, input);
+    [self setTitle: input];
+  }
+}
+
+NAN_GETTER(NUIAlertController::messageGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSString([self message]));
+  }
+}
+
+NAN_SETTER(NUIAlertController::messageSetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    declare_setter();
+    declare_pointer(NSString, input);
+    [self setMessage: input];
+  }
+}
+
+NAN_GETTER(NUIAlertController::preferredStyleGetter) {
+  JS_UNWRAP(UIAlertController, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UIAlertControllerStyle([self preferredStyle]));
+  }
+}
