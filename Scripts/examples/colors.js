e03aad0fc5332657e117350522971f3e211eeeb9
Emily Kolar <emkolar@gmail.com>
2019-07-06 07:37:16 -0500

    effect button toggles mode

--- Scripts/examples/colors.js
+++ Scripts/examples/colors.js
@@ -3,6 +3,7 @@ module.exports.fitbodDarkGrey = RGB(24, 26, 27);
 module.exports.fitbodMedGrey = RGB(29, 31, 32);
 module.exports.fitbodMedGrey = RGB(59, 66, 70);
 module.exports.fitbodLightGrey = RGB(59, 66, 70);
+module.exports.purple = RGB(105, 69, 209);
 module.exports.black = UIColor.black;
 module.exports.white = UIColor.white;
 module.exports.clear = UIColor.clear;
--- Scripts/examples/emojiCam/index.js
+++ Scripts/examples/emojiCam/index.js
@@ -11,7 +11,6 @@ const {
   SKLabelNode,
   SKScene,
   ARAnchor,
-  UITextField,
   UITextFieldDelegate,
   UIApplication,
   UIButton,
@@ -21,32 +20,30 @@ const {
   UIView,
   UISlider,
   UIStoryboard,
-  CoreGraphics,
   SKTexture,
   SKSpriteNode,
-  RPScreenRecorder,
-  RPPreviewViewController,
-  RPPreviewViewControllerDelegate,
   SKEmitterNode,
   SKNode,
   SKAction,
 } = SweetieKit;
 
 function emojiCam(nav) {
-  let manualTrackMsgHide = false;
+  let isEffectMode = false;
+  const nicTexture = SKTexture(UIImage('nic'));
+
+  let text = nicTexture;
 
   const vcId = 'emojicamVC';
-  const intoVcId = 'introVC';
-  const closeBtnTag = 'closeBtnTag';
   const topViewTag = 'topViewTag';
   const hideTrackingBtnTag = 'hideTrackingBtnTag';
   const trackingViewTag = 'trackingViewTag';
   const trackingLabelTag = 'trackingLabelTag';
+  const effectBtnTag = 'effectBtnTag';
 
   const sb = UIStoryboard.storyboardWithNameBundle('Main');
   const demoVC = sb.instantiateViewControllerWithIdentifier(vcId);
-  const introVC = sb.instantiateViewControllerWithIdentifier(intoVcId);
   const menuButton = demoVC.view.viewWithTag(1);
+  const effectButton = demoVC.view.viewWithStringTag(effectBtnTag);
   const sbTopView = demoVC.view.viewWithStringTag(topViewTag);
   const sbTextField = sbTopView.viewWithTag(2);
 
@@ -74,12 +71,20 @@ function emojiCam(nav) {
 
   hideTrackingMsgBtn.addTargetActionForControlEvents(() => {
     toggleTrackingMsgView(false);
-    }, UIControlEventTouchUpInside);
-  //let text = '👀';
-  let text = SKTexture(UIImage('nic'));
+  }, UIControlEventTouchUpInside);
+
+  effectButton.backgroundColor = colors.fitbodLightGrey;
+  effectButton.addTargetActionForControlEvents(() => {
+    isEffectMode = !isEffectMode;
+    if (isEffectMode) {
+      effectButton.backgroundColor = colors.purple;
+    } else {
+      effectButton.backgroundColor = colors.fitbodLightGrey;
+    }
+  }, UIControlEventTouchUpInside);
+
   UIImage.transparent = UIImage.transparent || UIImage('transparent');
 
-  let isEffectMode = false;
   const sparkParticlePath = 'Spark.sks';
   const magicParticlePath = 'Magic.sks';
 
@@ -94,23 +99,6 @@ function emojiCam(nav) {
     return s.split('\n').map(line => lineWrap(line)).join('\n');
   }
 
-  // function makeCamBtn(demoVC, btnSize) {
-  //   const camBtn = UIButton(CGRectMake(
-  //     (demoVC.view.frame.width - btnSize) / 2,
-  //     demoVC.view.frame.height - (124 + btnSize),
-  //     btnSize,
-  //     btnSize
-  //   ));
-  //   camBtn.layer.cornerRadius = btnSize / 2;
-  //   camBtn.layer.shadowOpacity = 1;
-  //   camBtn.layer.shadowOffset = { width: 0, height: 4 };
-  //   camBtn.layer.shadowColor = colors.fitbodDarkGrey;
-  //   camBtn.layer.shadowRadius = 6;
-  //   camBtn.showsTouchWhenHighlighted = true;
-  //   camBtn.setBackgroundImageForState(UIImage('camera_btn_thin'), UIControlStateNormal);
-  //   return camBtn;
-  // }
-
   const trackViewGradient = CAGradientLayer();
   trackViewGradient.frame = demoVC.view.bounds;
   trackViewGradient.colors = [
@@ -123,58 +111,6 @@ function emojiCam(nav) {
   trackingMsgView.layer.insertSublayerAtIndex(trackViewGradient, 0);
 
   trackingLabel.text = 'Establishing tracking';
-  // const attrText = NSMutableAttributedString('Establishing tracking');
-  // attrText.addAttribute(NSStrokeWidthAttributeName, 1, {
-  //   location: 0,
-  //   length: attrText.length,
-  // });
-  // attrText.addAttribute(NSStrokeColorAttributeName, colors.black, {
-  //   location: 0,
-  //   length: attrText.length,
-  // });
-  // attrText.addAttribute(NSForegroundColorAttributeName, colors.white, {
-  //   location: 0,
-  //   length: placeholderText.length,
-  // });
-  // trackingLabel.attributedText = attrText;
-
-  function makeTextField(demoVC, fieldHeight, horOffset, callback) {
-    const frame = CGRectMake(
-      horOffset,
-      0,
-      demoVC.view.frame.width - (horOffset * 2),
-      fieldHeight);
-
-    const field = UITextField.initNewWithFrameCallback(frame, callback);
-    field.textColor = {
-      ...colors.white,
-      alpha: 0.8,
-    };
-    field.backgroundColor = colors.clear;
-
-    const placeholderText = 'Enter some text...';
-    const placeholderFont = UIFont('Lato-Bold', 17);
-
-    const attrPlaceholder = NSMutableAttributedString(placeholderText);
-
-    attrPlaceholder.addAttribute(NSForegroundColorAttributeName, colors.white, {
-      location: 0,
-      length: placeholderText.length,
-    });
-    attrPlaceholder.addAttribute(NSFontAttributeName, placeholderFont, {
-      location: 0,
-      length: placeholderText.length,
-    });
-    attrPlaceholder.addAttribute(NSKernAttributeName, 1.1, {
-      location: 0,
-      length: placeholderText.length,
-    });
-    field.attributedPlaceholder = attrPlaceholder;
-    field.autocorrectionType = UITextAutocorrectionTypeNo;
-    field.spellCheckingType = UITextSpellCheckingTypeNo;
-
-    return field;
-  }
 
   function makeParticleButton(demoVC) {
     const size = 40;
@@ -187,112 +123,7 @@ function emojiCam(nav) {
     return btn;
   }
 
-  // function takeScreenshot(view) {
-  //   const w = view.frame.width;
-  //   const h = view.frame.height;
-  //
-  //   try {
-  //     CoreGraphics.UIGraphicsBeginImageContextWithOptions(
-  //       { width: w, height: h },
-  //       true,
-  //       0.0,
-  //     );
-  //
-  //     view.layer.renderInContext();
-  //
-  //     const img = CoreGraphics.UIGraphicsGetImageFromCurrentImageContext();
-  //
-  //     if (img) {
-  //       const ssFrame = { x: 0, y: 0, width: w * 0.4, height: h * 0.4 };
-  //       const ssView = UIImageView(img);
-  //       ssView.frame = ssFrame;
-  //       ssView.layer.borderWidth = 1;
-  //       ssView.layer.borderColor = UIColor.white;
-  //       view.addSubview(ssView);
-  //       view.bringSubviewToFront(ssView);
-  //
-  //       // ==========
-  //       // recalls UIApplicationMain and crashes?
-  //       // ==========
-  //
-  //       console.log('Writing UIImage');
-  //       UIKit.UIImageWriteToSavedPhotosAlbum(img, () => {
-  //         console.log('written!');
-  //       });
-  //     }
-  //   } finally {
-  //     CoreGraphics.UIGraphicsEndImageContext();
-  //   }
-  // }
-  //
-  // function getCapturedImage(arView) {
-  //   try {
-  //     const orientation = (new UIApplication()).statusBarOrientation;
-  //     const size = { width: arView.frame.width, height: arView.frame.height };
-  //     const xform = arView.session.currentFrame.displayTransform(orientation, size);
-  //     const xFormMatrix = new THREE.Matrix3().fromArray(xform);
-  //     const matrix = new THREE.Matrix3().rotate(90);
-  //     xFormMatrix.multiply(matrix);
-  //     const ciImg = arView.session.currentFrame.capturedImage;
-  //     const xformedImg = ciImg.imageByApplyingTransform(xFormMatrix);
-  //     console.log(xformedImg);
-  //     const uiImg = UIImage.alloc().initWithCIImageScaleOrientation(xformedImg, 1, orientation);
-  //     console.log(ciImg, xformedImg, uiImg);
-  //     const ssView = UIImageView(uiImg);
-  //     ssView.frame = { x: 0, y: 0, width: arView.frame.width, height: arView.frame.height };
-  //     return ssView;
-  //   } catch (err) {
-  //     console.log(err);
-  //     return undefined;
-  //   }
-  // }
-  //
-  // function toggleRecordScreen(demoVC, recorder) {
-  //   const del = new RPPreviewViewControllerDelegate();
-  //   del.previewControllerDidFinish = (previewController) => {
-  //     console.log('preview controller did finish - uidemos.js toggleRecordScreen()');
-  //     previewController.dismissViewControllerAnimatedCompletion(true, () => {
-  //       console.log('preview controller did finish 2 - uidemos.js toggleRecordScreen()');
-  //     });
-  //   };
-  //
-  //   if (recorder.isRecording) {
-  //     console.log('stop recording - uidemos.js toggleRecordScreen()');
-  //     recorder.stopRecordingWithHandler((previewController) => {
-  //       console.log('stop recording 2 - uidemos.js toggleRecordScreen()');
-  //       previewController.delegate = del;
-  //       demoVC.presentViewControllerAnimatedCompletion(previewController, true, () => {
-  //         console.log('stop recording 3 - uidemos.js toggleRecordScreen()');
-  //       });
-  //     });
-  //   } else if (recorder.isAvailable) {
-  //     console.log('start recording - uidemos.js toggleRecordScreen()');
-  //     recorder.startRecordingWithHandler(() => {});
-  //   }
-  // }
-
-  function makeTopView(demoVC, fieldHeight) {
-    const borderView = UIView({ x: 0, y: fieldHeight, width: demoVC.view.frame.width, height: 1 });
-    borderView.backgroundColor = {
-      ...colors.white,
-      alpha: 0.3,
-    };
-
-    const topView = UIView({ x: 0, y: (fieldHeight * 1.5), width: demoVC.view.frame.width, height: fieldHeight + 1 });
-    topView.backgroundColor = {
-      ...colors.white,
-      alpha: 0.1,
-    };
-
-    topView.addSubview(borderView);
-
-    return topView;
-  }
-
   async function make(nav, demoVC) {
-    // const recorder = new RPScreenRecorder();
-    // console.log(recorder);
-
     view = demoVC.view;
     arView = ARSKView({ x: 0, y: 0, width: view.frame.width, height: view.frame.height });
     config = ARWorldTrackingConfiguration();
@@ -355,7 +186,6 @@ function emojiCam(nav) {
             return char.node;
           }
         }
-        //throw new Error("couldn't find node for anchor " + anchor.identifier.UUIDString);
       }
     });
 
@@ -369,18 +199,10 @@ function emojiCam(nav) {
         const { currentFrame } = arView.session;
 
         if (currentFrame) {
-          const { trackingState, trackingStateReason } = currentFrame.camera;
-          if (trackingState === ARTrackingStateNotAvailable) {
-            console.log('tracking state: not available');
-          } else if (trackingState === ARTrackingStateLimited) {
-            console.log('tracking state: limited');
+          const { trackingState } = currentFrame.camera;
+          if (trackingState === ARTrackingStateLimited) {
             toggleTrackingMsgView(true);
-            console.log(trackingMsgView);
-            // if (!manualTrackMsgHide) {
-            //   toggleTrackingMsgView(true);
-            // }
           } else if (trackingState === ARTrackingStateNormal) {
-            console.log('tracking state: normal');
             toggleTrackingMsgView(false);
           }
         }
@@ -440,7 +262,6 @@ function emojiCam(nav) {
         };
 
         node.numberOfLines = textWrap(str).split('\n').length;
-        //node.preferredMaxLayoutWidth = 10;
 
         const attrTxt = NSMutableAttributedString(str);
         attrTxt.addAttribute(NSForegroundColorAttributeName, colors.white, attrRange);
@@ -496,7 +317,6 @@ function emojiCam(nav) {
       let pt = touches[0].locationInView(touch.view);
       pt.y -= Math.trunc(0.1*touch.view.height);
       let hits = arView.hitTestTypes(pt, 1);
-      //console.log('touchesMovedWithEvent', touches.length, pt);
       if (hits && hits.length > 0) {
         active = _update(hits ? hits[0] : null);
         console.log(active.node.xScale, active.node.yScale,
@@ -532,18 +352,9 @@ function emojiCam(nav) {
       active = null;
     };
 
-    /*
-    this.interval = setInterval(() => {
-      for (let c of chars) {
-        let pos = c.node.position;
-        pos.x += 1;
-        c.node.position = pos;
-      }
-    }, 1000/60);*/
-
     demoVC.view.addSubview(arView);
     arView.pinToSuperview();
-    // nav.pushViewControllerAnimated(demoVC, true);
+
     new UIApplication()
       .keyWindow
       .rootViewController
@@ -551,17 +362,6 @@ function emojiCam(nav) {
       .presentViewControllerAnimatedCompletion(demoVC, true, () => {});
 
     arView.presentScene(scene);
-
-    // const btnSize = 70;
-
-    // const camBtn = makeCamBtn(demoVC, btnSize);
-    // camBtn.addTargetActionForControlEvents(() => {
-    //   console.log('recording');
-    //   //toggleRecordScreen(demoVC, recorder);
-    //   // takeScreenshot(demoVC.view);
-    //   const imgView = getCapturedImage(arView);
-    //   if (imgView) demoVC.view.addSubview(imgView);
-    // }, UIControlEventTouchUpInside);
 
     const fireBtn = makeParticleButton(demoVC);
     fireBtn.addTargetActionForControlEvents(() => {
@@ -573,11 +373,17 @@ function emojiCam(nav) {
     const horOffset = 24;
 
     sbTextField.addTargetActionForControlEvents(() => {
-      text = sbTextField.text;
+      if (!sbTextField.text || !sbTextField.text.length) {
+        text = nicTexture;
+      } else {
+        text = sbTextField.text;
+      }
     }, UIControlEventAllEditingEvents);
+
     const fieldDel = new UITextFieldDelegate({
       textFieldShouldReturn: () => true,
     });
+
     sbTextField.delegate = fieldDel;
 
     const placeholderText = 'Enter some text';
@@ -592,42 +398,6 @@ function emojiCam(nav) {
       length: placeholderText.length,
     });
     sbTextField.attributedPlaceholder = attrPlaceholder;
-    // const field = makeTextField(demoVC, fieldHeight, horOffset, () => {
-    //   text = field.text;
-    // });
-
-    // const topView = makeTopView(demoVC, fieldHeight);
-    // demoVC.view.addSubview(topView);
-    /*
-    topView.pinToSuperviewWithInsetsEdges(
-      UIEdgeInsetsMake(0, 0, 0, 0),
-      UIRectEdgeTop | UIRectEdgeLeft | UIRectEdgeRight);
-      */
-
-    // topView.addSubview(field);
-    /*
-    field.pinToSuperviewWithInsetsEdges(
-      UIEdgeInsetsMake(0, 0, 0, fireBtn.width),
-      UIRectEdgeTop | UIRectEdgeLeft | UIRectEdgeBottom | UIRectEdgeRight);
-      */
-
-    // topView.addSubview(fireBtn);
-    /*
-    fireBtn.pinToSuperviewWithInsetsEdges(
-      UIEdgeInsetsMake(0, field.width, 0, 0),
-      UIRectEdgeTop | UIRectEdgeLeft | UIRectEdgeBottom | UIRectEdgeRight);
-      */
-
-    // noinspection JSSuspiciousNameCombination
-    // const closeBtn = UIButton({ x: 0, y: fieldHeight * 0.5, width: fieldHeight, height: fieldHeight });
-    // closeBtn.setTitleForState('x', UIControlStateNormal);
-    // closeBtn.setTitleColorForState(colors.white, UIControlStateNormal);
-    // closeBtn.backgroundColor = colors.clear;
-    // closeBtn.addTargetActionForControlEvents(() => {
-    //   demoVC.dismissViewControllerAnimatedCompletion(true, () => {});
-    // }, UIControlEventTouchUpInside);
-    // closeBtn.alpha = 0.6;
-    // demoVC.view.addSubview(closeBtn);
 
     const viewW = view.frame.width;
     const scaleSliderY = (fieldHeight * 2) + 40;
@@ -636,12 +406,8 @@ function emojiCam(nav) {
     scaleSlider = UISlider({
       x: horOffset, y: scaleSliderY, width: viewW - (horOffset * 2), height: sliderHeight,
     });
-    demoVC.view.addSubview(scaleSlider);
-    /*
-    scaleSlider.pinToSuperviewWithInsetsEdges(
-      UIEdgeInsetsMake(scaleSliderY, 0, 0, 0),
-      UIRectEdgeTop | UIRectEdgeLeft | UIRectEdgeRight);
-      */
+    // demoVC.view.addSubview(scaleSlider);
+
     scaleSlider.value = 0.25;
     scaleSlider.setThumbImageForState(UIImage.transparent, UIControlStateNormal);
     scaleSlider.addTargetActionForControlEvents(() => {
@@ -672,9 +438,10 @@ function emojiCam(nav) {
     }, UIControlEventValueChanged);
 
     menuButton.layer.cornerRadius = menuButton.frame.height / 2;
-    const subviews = [scaleSlider, sbTopView, menuButton, trackingMsgView]; /*, distSlider, rotSlider, camBtn */
+    effectButton.layer.cornerRadius = menuButton.frame.height / 2;
+
+    const subviews = [sbTopView, menuButton, effectButton, trackingMsgView]; /*, distSlider, rotSlider, camBtn */
     subviews.forEach((s) => {
-      //demoVC.view.addSubview(s);
       demoVC.view.bringSubviewToFront(s);
     });
 
