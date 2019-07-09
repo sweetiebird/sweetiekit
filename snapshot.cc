e67ff49cc36aeb14cd4019dec62d3315800020d4
Shawn Presser <shawnpresser@gmail.com>
2019-07-09 00:46:27 -0500

    UIKitForMac snapshot WIP

--- snapshot.cc
+++ snapshot.cc
@@ -1,7 +1,13 @@
+#if __IPHONEOS__
 #if !NODE_IOS_SIMULATOR
 #ifdef DEBUG
 #include "./Debug/snapshot.cc"
 #else
 #include "./Release/snapshot.cc"
 #endif
+#endif
+#endif
+
+#if TARGET_OS_UIKITFORMAC
+#include "/Volumes/birdie/opt/native/node-uikitformac/out/Release/obj.target/v8_snapshot/geni/snapshot.cc"
 #endif
