e47022818cbd152c409b870c6b3c132c48a80aee
Shawn Presser <shawnpresser@gmail.com>
2019-07-10 18:24:12 -0500

    Update AnimalWheel example

--- Scripts/examples/AnimalWheel/index.js
+++ Scripts/examples/AnimalWheel/index.js
@@ -1,5 +1,5 @@
 const SweetieKit = require('std:sweetiekit.node');
-const axios = require('axios');
+axios = require('axios');
 
 THREE = require('../../vendor/three/three');
 
@@ -16,9 +16,9 @@ const {
   UIImage,
 } = SweetieKit;
 
-const animalImagesCache = {};
+AnimalWheel_animalImagesCache = {};
 
-function makeAnimalNodes(animals) {
+AnimalWheel_makeAnimalNodes = function AnimalWheel_makeAnimalNodes(animals) {
   return animals.map((animal) => {
     const cardGeo = SCNBox({ width: 0.25, height: 0.35, length: 0.01, chamferRadius: 0 });
     const animalNode = new SCNNode(cardGeo);
@@ -27,11 +27,11 @@ function makeAnimalNodes(animals) {
   });
 }
 
-async function getImage(name, selected = false) {
+AnimalWheel_getImage = async function AnimalWheel_getImage(name, selected = false) {
   const imgKey = `${name}${selected ? '_selected' : ''}`;
 
-  if (animalImagesCache[imgKey]) {
-    return animalImagesCache[imgKey];
+  if (AnimalWheel_animalImagesCache[imgKey]) {
+    return AnimalWheel_animalImagesCache[imgKey];
   }
 
   const imgUrl = `https://emkolar.ninja/sweetiekit/img/${imgKey}.png`;
@@ -45,7 +45,7 @@ async function getImage(name, selected = false) {
 
     const cardImage = UIImage.imageWithData(response.data);
 
-    animalImagesCache[imgKey] = cardImage;
+    AnimalWheel_animalImagesCache[imgKey] = cardImage;
 
     return cardImage;
   } catch (err) {
@@ -53,9 +53,9 @@ async function getImage(name, selected = false) {
   }
 }
 
-function setMaterials(nodes, selected = false) {
+AnimalWheel_setMaterials = function AnimalWheel_setMaterials(nodes, selected = false) {
   nodes.forEach(async (n) => {
-    const cardImage = await getImage(n.name, selected);
+    const cardImage = await AnimalWheel_getImage(n.name, selected);
 
     if (cardImage) {
       const cardMat = new SCNMaterial();
@@ -65,7 +65,7 @@ function setMaterials(nodes, selected = false) {
   });
 }
 
-async function playSound(nodeName) {
+AnimalWheel_playSound = async function AnimalWheel_playSound(nodeName) {
   try {
     const soundUrl = `https://emkolar.ninja/sweetiekit/sound/${nodeName}.mp3`;
 
@@ -89,7 +89,7 @@ async function playSound(nodeName) {
   }
 }
 
-async function getAnimalsList() {
+AnimalWheel_getAnimalsList = async function AnimalWheel_getAnimalsList() {
   const dataUrl = 'https://emkolar.ninja/sweetiekit/data/animals.json';
 
   try {
@@ -105,22 +105,23 @@ async function getAnimalsList() {
   }
 }
 
-async function makeWheel(nodes) {
+AnimalWheel_makeWheel = async function AnimalWheel_makeWheel(nodes) {
   const len = nodes.length;
   const radius = 0.5;
   const step = (2.0 * Math.PI) / len;
 
   for (let i = 0; i < len; i++) {
     const n = nodes[i];
-    const xPos = Math.cos(step * i) * radius;
-    const zPos = Math.sin(step * i) * radius;
+    const angle = step * i;
+    const xPos = Math.cos(angle) * radius;
+    const zPos = -Math.sin(angle) * radius;
     n.position = { x: xPos, y: 0, z: zPos };
-    n.rotation = { x: 0, y: 0, z: 0, w: -Math.PI * (i/6/7.5) };
+    n.eulerAngles = { x: 0, y: angle + Math.PI/4, z: 0 };
   }
-}
+};
 
-async function make(nav, demoVC) {
-  const animals = await getAnimalsList();
+AnimalWheel_make = async function AnimalWheel_make(nav, demoVC) {
+  const animals = await AnimalWheel_getAnimalsList();
 
   audioSession = AVAudioSession.sharedInstance();
   audioSession.setCategoryError(AVAudioSessionCategoryPlayback);
@@ -129,6 +130,7 @@ async function make(nav, demoVC) {
 
   view = demoVC.view;
   arView = new ARSCNView({ x: 0, y: 0, width: view.frame.width, height: view.frame.height });
+  arView.autoenablesDefaultLighting = true;
   config = new ARWorldTrackingConfiguration();
   viewDel = ARSCNViewDelegate({
     rendererNodeForAnchor: () => {
@@ -136,24 +138,58 @@ async function make(nav, demoVC) {
     },
   });
   scene = new SCNScene();
-  animalNodes = makeAnimalNodes(animals);
+  animalNodes = AnimalWheel_makeAnimalNodes(animals);
 
   containerNode = SCNNode();
-  containerNode.position = { x: 0, y: 0, z: -2 };
+  containerNode.position = { x: 0, y: 0, z: -1 };
 
   animalNodes.forEach((n) => {
     containerNode.addChildNode(n);
   });
 
+  /*
+  // Disable automatic lighting
+  arView.autoenablesDefaultLighting = false
+  arView.automaticallyUpdatesLighting = false
+
+  // create ambient light
+  ambientLightNode.light = SCNLight()
+  ambientLightNode.light.type = SCNLightTypeAmbient;
+  ambientLightNode.light.color = UIColor.darkGray;
+  scene.rootNode.addChildNode(ambientLightNode);
+
+  // create secondary light
+  lightNode = SCNNode();
+  lightNode.light = SCNLight()
+  lightNode.light.type = SCNLightTypeOmni;
+  lightNode.position = SCNVector3Make(0, 1, 1);
+  scene.rootNode.addChildNode(lightNode);
+  
+  // create main light that cast shadow
+  lightNode2 = SCNNode();
+  lightNode2.light = SCNLight()
+  lightNode2.light.type = SCNlightTypeSpot;
+  lightNode2.position = SCNVector3Make(-1, 10, 1);
+  lightNode2.eulerAngles = SCNVector3Make(-Math.PI*0.5, 0, 0);
+  lightNode2.light.castsShadow = true; // to cast shadow 
+  lightNode2.light.shadowMode = SCNShadowModeDeferred; // to render shadow in transparent plane
+  lightNode2.light.shadowSampleCount = 64; //remove flickering of shadow and soften shadow
+  lightNode2.light.shadowMapSize = CGSizeMake(2048, 2048); //sharpen or detail shadow
+  containerNode.addChildNode(lightNode2)
+
+  // place shadow plane under your object
+  shadowPlane = SCNPlane(10, 10);
+//  shadowPlane.materials.first.colorBufferWriteMask = 0;
+  shadowPlaneNode = SCNNode(shadowPlane);
+  planeNode.eulerAngles = SCNVector3Make(-Math.PI*0.5, 0, 0); // because plane is created vertical
+  containerNode.addChildNode(shadowPlaneNode);
+   */
+
   arView.delegate = viewDel;
   arView.scene = scene;
 
-  spotlight = new SCNLight();
-  spotlight.type = SCNLightTypeOmni;
-
-  scene.rootNode.light = spotlight;
-  scene.rootNode.position = { x: 1, y: 1, z: 0 };
-  scene.rootNode.eulerAngles = { x: Math.PI / 2, y: 0, z: 0 };
+  scene.rootNode.position = SCNVector3Make(1, 1, 0);
+  scene.rootNode.eulerAngles = SCNVector3Make(Math.PI*0.5, 0, 0);
 
   demoVC.view.addSubview(arView);
   nav.pushViewControllerAnimated(demoVC, true);
@@ -176,7 +212,7 @@ async function make(nav, demoVC) {
       }
 
       if (hit.node) {
-        playSound(hit.node.name);
+        AnimalWheel_playSound(hit.node.name);
 
         lastNode = hit.node.name;
       }
@@ -191,12 +227,16 @@ async function make(nav, demoVC) {
         setTimeout(configure, 10);
       } else {
         scene.rootNode.addChildNode(containerNode);
-        await makeWheel(animalNodes);
-        setMaterials(animalNodes);
+        await AnimalWheel_makeWheel(animalNodes);
+        AnimalWheel_setMaterials(animalNodes);
       }
     }
     configure();
   }
+}
+
+function make(...args) {
+  return AnimalWheel_make(...args);
 }
 
 module.exports = make;
