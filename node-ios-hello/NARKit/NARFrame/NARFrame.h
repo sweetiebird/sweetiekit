ab248afecbd2969e643a07ed630dbef2e47b1bc9
Shawn Presser <shawnpresser@gmail.com>
2019-07-10 21:01:32 -0500

    Re-expose ARFrame

--- node-ios-hello/NARKit/NARFrame/NARFrame.h
+++ node-ios-hello/NARKit/NARFrame/NARFrame.h
@@ -12,15 +12,36 @@
 
 #if TARGET_OS_IPHONE
 
+#define js_value_ARSegmentationClass(x) JS_ENUM(ARSegmentationClass, uint8_t, x)
+#define to_value_ARSegmentationClass(x) TO_ENUM(ARSegmentationClass, uint8_t, x)
+#define is_value_ARSegmentationClass(x) IS_ENUM(ARSegmentationClass, uint8_t, x)
+
+#define js_value_ARWorldMappingStatus(x) JS_ENUM(ARWorldMappingStatus, NSInteger, x)
+#define to_value_ARWorldMappingStatus(x) TO_ENUM(ARWorldMappingStatus, NSInteger, x)
+#define is_value_ARWorldMappingStatus(x) IS_ENUM(ARWorldMappingStatus, NSInteger, x)
+
 #define js_value_ARFrame(x) js_value_wrapper(x, ARFrame)
 #define to_value_ARFrame(x) to_value_wrapper(x, ARFrame)
 #define is_value_ARFrame(x) is_value_wrapper(x, ARFrame)
 
 JS_WRAP_CLASS(ARFrame, NSObject);
-  JS_METHOD(displayTransform);
-  JS_PROP(camera);
-  JS_PROP(lightEstimate);
-  JS_PROP(capturedImage);
+  JS_METHOD(hitTestTypes);
+  JS_METHOD(raycastQueryFromPointAllowingTargetAlignment);
+  JS_METHOD(displayTransformForOrientationViewportSize);
+  JS_PROP_READONLY(timestamp);
+  JS_PROP_READONLY(capturedImage);
+  JS_PROP_READONLY(cameraGrainTexture);
+  JS_PROP_READONLY(cameraGrainIntensity);
+  JS_PROP_READONLY(capturedDepthData);
+  JS_PROP_READONLY(capturedDepthDataTimestamp);
+  JS_PROP_READONLY(camera);
+  JS_PROP_READONLY(anchors);
+  JS_PROP_READONLY(lightEstimate);
+  JS_PROP_READONLY(rawFeaturePoints);
+  JS_PROP_READONLY(worldMappingStatus);
+  JS_PROP_READONLY(segmentationBuffer);
+  JS_PROP_READONLY(estimatedDepthData);
+  JS_PROP_READONLY(detectedBody);
 JS_WRAP_CLASS_END(ARFrame);
 
 #endif
