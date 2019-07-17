95fa17c9e4004336b6ae765eb9810556c10975dd
Shawn Presser <shawnpresser@gmail.com>
2019-07-16 23:04:26 -0500

    Update snapshot for uikitformac

--- snapshot.cc
+++ snapshot.cc
@@ -1,13 +1,14 @@
-#if TARGET_OS_IPHONE && !TARGET_OS_UIKITFORMAC
+#include <Availability.h>
+
+#if TARGET_OS_UIKITFORMAC
+#include "./Snapshot/uikitformac/Release/snapshot.cc"
+#else
 #if !NODE_IOS_SIMULATOR
 #ifdef DEBUG
 #include "./Debug/snapshot.cc"
 #else
-#include "./Release/snapshot.cc"
+#include "./Debug/snapshot.cc"
+//#include "./Release/snapshot.cc"
 #endif
 #endif
-#endif
-
-#if TARGET_OS_UIKITFORMAC
-#include "/Volumes/birdie/opt/native/node-uikitformac/out/Release/obj.target/v8_snapshot/geni/snapshot.cc"
 #endif
