4fff4b18587ca4e5a4db37ac09801b18591739fd
Shawn Presser <shawnpresser@gmail.com>
2019-07-26 02:45:50 -0500

    Re-expose UITouch

--- node-ios-hello/NUIKit/NUITouch/NUITouch.h
+++ node-ios-hello/NUIKit/NUITouch/NUITouch.h
@@ -31,73 +31,27 @@
 #define is_value_UITouchProperties(x) IS_OPTS(UITouchProperties, NSInteger, x)
 
 JS_WRAP_CLASS(UITouch, NSObject);
-  
-  //@property(nonatomic,readonly) NSTimeInterval      timestamp;
-  JS_PROP(timestamp);
-  //@property(nonatomic,readonly) UITouchPhase        phase;
-  JS_PROP(phase);
-  //@property(nonatomic,readonly) NSUInteger          tapCount;   // touch down within a certain point within a certain amount of time
-  JS_PROP(tapCount);
-  //@property(nonatomic,readonly) UITouchType         type NS_AVAILABLE_IOS(9_0);
-  JS_PROP(type);
-
-  // majorRadius and majorRadiusTolerance are in points
-  // The majorRadius will be accurate +/- the majorRadiusTolerance
-  //@property(nonatomic,readonly) CGFloat majorRadius NS_AVAILABLE_IOS(8_0);
-  JS_PROP(majorRadius);
-  //@property(nonatomic,readonly) CGFloat majorRadiusTolerance NS_AVAILABLE_IOS(8_0);
-  JS_PROP(majorRadiusTolerance);
-
-  //@property(nullable,nonatomic,readonly,strong) UIWindow                        *window;
-  JS_PROP(window);
-  //@property(nullable,nonatomic,readonly,strong) UIView                          *view;
-  JS_PROP(view);
-  //@property(nullable,nonatomic,readonly,copy)   NSArray <UIGestureRecognizer *> *gestureRecognizers NS_AVAILABLE_IOS(3_2);
-
-  //- (CGPoint)locationInView:(nullable UIView *)view;
   JS_METHOD(locationInView);
-  //- (CGPoint)previousLocationInView:(nullable UIView *)view;
   JS_METHOD(previousLocationInView);
-
-  // Use these methods to gain additional precision that may be available from touches.
-  // Do not use precise locations for hit testing. A touch may hit test inside a view, yet have a precise location that lies just outside.
-  //- (CGPoint)preciseLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
   JS_METHOD(preciseLocationInView);
-  //- (CGPoint)precisePreviousLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
   JS_METHOD(precisePreviousLocationInView);
-
-  // Force of the touch, where 1.0 represents the force of an average touch
-  //@property(nonatomic,readonly) CGFloat force NS_AVAILABLE_IOS(9_0);
-  JS_PROP(force);
-  // Maximum possible force with this input mechanism
-  //@property(nonatomic,readonly) CGFloat maximumPossibleForce NS_AVAILABLE_IOS(9_0);
-  JS_PROP(maximumPossibleForce);
-
-  // Azimuth angle. Valid only for stylus touch types. Zero radians points along the positive X axis.
-  // Passing a nil for the view parameter will return the azimuth relative to the touch's window.
-  //- (CGFloat)azimuthAngleInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-  // A unit vector that points in the direction of the azimuth angle. Valid only for stylus touch types.
-  // Passing nil for the view parameter will return a unit vector relative to the touch's window.
-  //- (CGVector)azimuthUnitVectorInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-
-  // Altitude angle. Valid only for stylus touch types.
-  // Zero radians indicates that the stylus is parallel to the screen surface,
-  // while M_PI/2 radians indicates that it is normal to the screen surface.
-  //@property(nonatomic,readonly) CGFloat altitudeAngle NS_AVAILABLE_IOS(9_1);
-
-  // An index which allows you to correlate updates with the original touch.
-  // Is only guaranteed non-nil if this UITouch expects or is an update.
-  //@property(nonatomic,readonly) NSNumber * _Nullable estimationUpdateIndex NS_AVAILABLE_IOS(9_1);
-  JS_PROP(estimationUpdateIndex);
-  // A set of properties that has estimated values
-  // Only denoting properties that are currently estimated
-  //@property(nonatomic,readonly) UITouchProperties estimatedProperties NS_AVAILABLE_IOS(9_1);
-  JS_PROP(estimatedProperties);
-  // A set of properties that expect to have incoming updates in the future.
-  // If no updates are expected for an estimated property the current value is our final estimate.
-  // This happens e.g. for azimuth/altitude values when entering from the edges
-  //@property(nonatomic,readonly) UITouchProperties estimatedPropertiesExpectingUpdates NS_AVAILABLE_IOS(9_1);
-  JS_PROP(estimatedPropertiesExpectingUpdates);
+  JS_METHOD(azimuthAngleInView);
+  JS_METHOD(azimuthUnitVectorInView);
+  JS_PROP_READONLY(timestamp);
+  JS_PROP_READONLY(phase);
+  JS_PROP_READONLY(tapCount);
+  JS_PROP_READONLY(type);
+  JS_PROP_READONLY(majorRadius);
+  JS_PROP_READONLY(majorRadiusTolerance);
+  JS_PROP_READONLY(window);
+  JS_PROP_READONLY(view);
+  JS_PROP_READONLY(gestureRecognizers);
+  JS_PROP_READONLY(force);
+  JS_PROP_READONLY(maximumPossibleForce);
+  JS_PROP_READONLY(altitudeAngle);
+  JS_PROP_READONLY(estimationUpdateIndex);
+  JS_PROP_READONLY(estimatedProperties);
+  JS_PROP_READONLY(estimatedPropertiesExpectingUpdates);
 JS_WRAP_CLASS_END(UITouch);
 
 #endif /* NUITouch_h */
--- node-ios-hello/NUIKit/NUITouch/NUITouch.mm
+++ node-ios-hello/NUIKit/NUITouch/NUITouch.mm
@@ -5,80 +5,37 @@
 //  Copyright © 2019 sweetiebird. All rights reserved.
 //
 #include "NUITouch.h"
-#include "NUIView.h"
+
+#define instancetype UITouch
+#define js_value_instancetype js_value_UITouch
 
 NUITouch::NUITouch() {}
 NUITouch::~NUITouch() {}
 
 JS_INIT_CLASS(UITouch, NSObject);
-  // instance members (proto)
-  //@property(nonatomic,readonly) NSTimeInterval      timestamp;
-  JS_ASSIGN_PROTO_PROP_READONLY(timestamp);
-  //@property(nonatomic,readonly) UITouchPhase        phase;
-  JS_ASSIGN_PROTO_PROP_READONLY(phase);
-  //@property(nonatomic,readonly) NSUInteger          tapCount;   // touch down within a certain point within a certain amount of time
-  JS_ASSIGN_PROTO_PROP_READONLY(tapCount);
-  //@property(nonatomic,readonly) UITouchType         type NS_AVAILABLE_IOS(9_0);
-  JS_ASSIGN_PROTO_PROP_READONLY(type);
-
-  // majorRadius and majorRadiusTolerance are in points
-  // The majorRadius will be accurate +/- the majorRadiusTolerance
-  //@property(nonatomic,readonly) CGFloat majorRadius NS_AVAILABLE_IOS(8_0);
-  JS_ASSIGN_PROTO_PROP_READONLY(majorRadius);
-  //@property(nonatomic,readonly) CGFloat majorRadiusTolerance NS_AVAILABLE_IOS(8_0);
-  JS_ASSIGN_PROTO_PROP_READONLY(majorRadiusTolerance);
-
-  //@property(nullable,nonatomic,readonly,strong) UIWindow                        *window;
-  JS_ASSIGN_PROTO_PROP_READONLY(window);
-  //@property(nullable,nonatomic,readonly,strong) UIView                          *view;
-  JS_ASSIGN_PROTO_PROP_READONLY(view);
-  //@property(nullable,nonatomic,readonly,copy)   NSArray <UIGestureRecognizer *> *gestureRecognizers NS_AVAILABLE_IOS(3_2);
-
-  //- (CGPoint)locationInView:(nullable UIView *)view;
   JS_ASSIGN_PROTO_METHOD(locationInView);
-  //- (CGPoint)previousLocationInView:(nullable UIView *)view;
   JS_ASSIGN_PROTO_METHOD(previousLocationInView);
-
-  // Use these methods to gain additional precision that may be available from touches.
-  // Do not use precise locations for hit testing. A touch may hit test inside a view, yet have a precise location that lies just outside.
-  //- (CGPoint)preciseLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
   JS_ASSIGN_PROTO_METHOD(preciseLocationInView);
-  //- (CGPoint)precisePreviousLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
   JS_ASSIGN_PROTO_METHOD(precisePreviousLocationInView);
-
-  // Force of the touch, where 1.0 represents the force of an average touch
-  //@property(nonatomic,readonly) CGFloat force NS_AVAILABLE_IOS(9_0);
+  JS_ASSIGN_PROTO_METHOD(azimuthAngleInView);
+  JS_ASSIGN_PROTO_METHOD(azimuthUnitVectorInView);
+  JS_ASSIGN_PROTO_PROP_READONLY(timestamp);
+  JS_ASSIGN_PROTO_PROP_READONLY(phase);
+  JS_ASSIGN_PROTO_PROP_READONLY(tapCount);
+  JS_ASSIGN_PROTO_PROP_READONLY(type);
+  JS_ASSIGN_PROTO_PROP_READONLY(majorRadius);
+  JS_ASSIGN_PROTO_PROP_READONLY(majorRadiusTolerance);
+  JS_ASSIGN_PROTO_PROP_READONLY(window);
+  JS_ASSIGN_PROTO_PROP_READONLY(view);
+  JS_ASSIGN_PROTO_PROP_READONLY(gestureRecognizers);
   JS_ASSIGN_PROTO_PROP_READONLY(force);
-  // Maximum possible force with this input mechanism
-  //@property(nonatomic,readonly) CGFloat maximumPossibleForce NS_AVAILABLE_IOS(9_0);
   JS_ASSIGN_PROTO_PROP_READONLY(maximumPossibleForce);
-
-  // Azimuth angle. Valid only for stylus touch types. Zero radians points along the positive X axis.
-  // Passing a nil for the view parameter will return the azimuth relative to the touch's window.
-  //- (CGFloat)azimuthAngleInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-  // A unit vector that points in the direction of the azimuth angle. Valid only for stylus touch types.
-  // Passing nil for the view parameter will return a unit vector relative to the touch's window.
-  //- (CGVector)azimuthUnitVectorInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-
-  // Altitude angle. Valid only for stylus touch types.
-  // Zero radians indicates that the stylus is parallel to the screen surface,
-  // while M_PI/2 radians indicates that it is normal to the screen surface.
-  //@property(nonatomic,readonly) CGFloat altitudeAngle NS_AVAILABLE_IOS(9_1);
-
-  // An index which allows you to correlate updates with the original touch.
-  // Is only guaranteed non-nil if this UITouch expects or is an update.
-  //@property(nonatomic,readonly) NSNumber * _Nullable estimationUpdateIndex NS_AVAILABLE_IOS(9_1);
+  JS_ASSIGN_PROTO_PROP_READONLY(altitudeAngle);
   JS_ASSIGN_PROTO_PROP_READONLY(estimationUpdateIndex);
-  // A set of properties that has estimated values
-  // Only denoting properties that are currently estimated
-  //@property(nonatomic,readonly) UITouchProperties estimatedProperties NS_AVAILABLE_IOS(9_1);
   JS_ASSIGN_PROTO_PROP_READONLY(estimatedProperties);
-  // A set of properties that expect to have incoming updates in the future.
-  // If no updates are expected for an estimated property the current value is our final estimate.
-  // This happens e.g. for azimuth/altitude values when entering from the edges
-  //@property(nonatomic,readonly) UITouchProperties estimatedPropertiesExpectingUpdates NS_AVAILABLE_IOS(9_1);
   JS_ASSIGN_PROTO_PROP_READONLY(estimatedPropertiesExpectingUpdates);
 
+  // instance members (proto)
   // static members (ctor)
   JS_INIT_CTOR(UITouch, NSObject);
 
@@ -116,202 +73,185 @@ JS_INIT_CLASS_END(UITouch, NSObject);
 
 NAN_METHOD(NUITouch::New) {
   JS_RECONSTRUCT(UITouch);
-
-  Local<Object> obj = info.This();
-
-  NUITouch *ui = new NUITouch();
-
-  if (info[0]->IsExternal()) {
-    ui->SetNSObject((__bridge UITouch *)(info[0].As<External>()->Value()));
-  } else {
-    @autoreleasepool {
-      ui->SetNSObject([[UITouch alloc] init]);
+  @autoreleasepool {
+    UITouch* self = nullptr;
+    
+    if (info[0]->IsExternal()) {
+      self = (__bridge UITouch *)(info[0].As<External>()->Value());
+    } else if (info.Length() <= 0) {
+      self = [[UITouch alloc] init];
+    }
+    if (self) {
+      NUITouch *wrapper = new NUITouch();
+      wrapper->SetNSObject(self);
+      Local<Object> obj(info.This());
+      wrapper->Wrap(obj);
+      JS_SET_RETURN(obj);
+    } else {
+      Nan::ThrowError("UITouch::New: invalid arguments");
     }
   }
-  ui->Wrap(obj);
-
-  JS_SET_RETURN(obj);
 }
-
-//@property(nonatomic,readonly) NSTimeInterval      timestamp;
-JS_GETTER(UITouch, ui, timestamp,
-  JS_SET_RETURN(JS_NUM([ui timestamp]))
-);
-//@property(nonatomic,readonly) UITouchPhase        phase;
-JS_GETTER(UITouch, ui, phase,
-  JS_SET_RETURN(JS_INT([ui phase]))
-);
-//@property(nonatomic,readonly) NSUInteger          tapCount;   // touch down within a certain point within a certain amount of time
-JS_GETTER(UITouch, ui, tapCount,
-  JS_SET_RETURN(JS_INT([ui tapCount]))
-);
-//@property(nonatomic,readonly) UITouchType         type NS_AVAILABLE_IOS(9_0);
-JS_GETTER(UITouch, ui, type,
-  JS_SET_RETURN(JS_INT([ui type]))
-);
-
-// majorRadius and majorRadiusTolerance are in points
-// The majorRadius will be accurate +/- the majorRadiusTolerance
-//@property(nonatomic,readonly) CGFloat majorRadius NS_AVAILABLE_IOS(8_0);
-JS_GETTER(UITouch, ui, majorRadius,
-  JS_SET_RETURN(JS_NUM([ui majorRadius]))
-);
-//@property(nonatomic,readonly) CGFloat majorRadiusTolerance NS_AVAILABLE_IOS(8_0);
-JS_GETTER(UITouch, ui, majorRadiusTolerance,
-  JS_SET_RETURN(JS_NUM([ui majorRadiusTolerance]))
-);
-
-#include "NUIWindow.h"
-//@property(nullable,nonatomic,readonly,strong) UIWindow                        *window;
-JS_GETTER(UITouch, ui, window,
-  JS_SET_RETURN(JS_WRAPPER([ui window], UIWindow))
-);
 
 #include "NUIView.h"
-//@property(nullable,nonatomic,readonly,strong) UIView                          *view;
-JS_GETTER(UITouch, ui, view,
-  JS_SET_RETURN(JS_WRAPPER([ui view], UIView))
-);
 
-//@property(nullable,nonatomic,readonly,copy)   NSArray <UIGestureRecognizer *> *gestureRecognizers NS_AVAILABLE_IOS(3_2);
-
-
-//- (CGPoint)locationInView:(nullable UIView *)view;
 NAN_METHOD(NUITouch::locationInView) {
-  Nan::HandleScope scope;
-
-  JS_UNWRAP(UITouch, ui);
-  
-  NUIView *view = ObjectWrap::Unwrap<NUIView>(JS_OBJ(info[0]));
-
-  __block double x = 0;
-  __block double y = 0;
-  @autoreleasepool {
-    CGPoint loc = [ui locationInView:view->As<UIView>()];
-    x = loc.x;
-    y = loc.y;
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGPoint([self locationInView: view]));
   }
-  
-  Local<Object> result = Object::New(Isolate::GetCurrent());
-  result->Set(JS_STR("x"), JS_NUM(x));
-  result->Set(JS_STR("y"), JS_NUM(y));
-  
-  JS_SET_RETURN(result);
 }
 
-//- (CGPoint)previousLocationInView:(nullable UIView *)view;
 NAN_METHOD(NUITouch::previousLocationInView) {
-  Nan::HandleScope scope;
-
-  JS_UNWRAP(UITouch, ui);
-  
-  NUIView *view = ObjectWrap::Unwrap<NUIView>(JS_OBJ(info[0]));
-
-  __block double x = 0;
-  __block double y = 0;
-  @autoreleasepool {
-    CGPoint loc = [ui previousLocationInView:view->As<UIView>()];
-    x = loc.x;
-    y = loc.y;
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGPoint([self previousLocationInView: view]));
   }
-  
-  Local<Object> result = Object::New(Isolate::GetCurrent());
-  result->Set(JS_STR("x"), JS_NUM(x));
-  result->Set(JS_STR("y"), JS_NUM(y));
-  
-  JS_SET_RETURN(result);
 }
 
-// Use these methods to gain additional precision that may be available from touches.
-// Do not use precise locations for hit testing. A touch may hit test inside a view, yet have a precise location that lies just outside.
-//- (CGPoint)preciseLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
 NAN_METHOD(NUITouch::preciseLocationInView) {
-  Nan::HandleScope scope;
-
-  JS_UNWRAP(UITouch, ui);
-  
-  NUIView *view = ObjectWrap::Unwrap<NUIView>(JS_OBJ(info[0]));
-
-  __block double x = 0;
-  __block double y = 0;
-  @autoreleasepool {
-    CGPoint loc = [ui preciseLocationInView:view->As<UIView>()];
-    x = loc.x;
-    y = loc.y;
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGPoint([self preciseLocationInView: view]));
   }
-  
-  Local<Object> result = Object::New(Isolate::GetCurrent());
-  result->Set(JS_STR("x"), JS_NUM(x));
-  result->Set(JS_STR("y"), JS_NUM(y));
-  
-  JS_SET_RETURN(result);
 }
 
-//- (CGPoint)precisePreviousLocationInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
 NAN_METHOD(NUITouch::precisePreviousLocationInView) {
-  Nan::HandleScope scope;
-
-  JS_UNWRAP(UITouch, ui);
-  
-  NUIView *view = ObjectWrap::Unwrap<NUIView>(JS_OBJ(info[0]));
-
-  __block double x = 0;
-  __block double y = 0;
-  @autoreleasepool {
-    CGPoint loc = [ui precisePreviousLocationInView:view->As<UIView>()];
-    x = loc.x;
-    y = loc.y;
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGPoint([self precisePreviousLocationInView: view]));
   }
-  
-  Local<Object> result = Object::New(Isolate::GetCurrent());
-  result->Set(JS_STR("x"), JS_NUM(x));
-  result->Set(JS_STR("y"), JS_NUM(y));
-  
-  JS_SET_RETURN(result);
 }
 
-// Force of the touch, where 1.0 represents the force of an average touch
-//@property(nonatomic,readonly) CGFloat force NS_AVAILABLE_IOS(9_0);
-JS_GETTER(UITouch, ui, force,
-  JS_SET_RETURN(JS_NUM([ui force]))
-);
-// Maximum possible force with this input mechanism
-//@property(nonatomic,readonly) CGFloat maximumPossibleForce NS_AVAILABLE_IOS(9_0);
-JS_GETTER(UITouch, ui, maximumPossibleForce,
-  JS_SET_RETURN(JS_NUM([ui maximumPossibleForce]))
-);
-
-// Azimuth angle. Valid only for stylus touch types. Zero radians points along the positive X axis.
-// Passing a nil for the view parameter will return the azimuth relative to the touch's window.
-//- (CGFloat)azimuthAngleInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-// A unit vector that points in the direction of the azimuth angle. Valid only for stylus touch types.
-// Passing nil for the view parameter will return a unit vector relative to the touch's window.
-//- (CGVector)azimuthUnitVectorInView:(nullable UIView *)view NS_AVAILABLE_IOS(9_1);
-
-// Altitude angle. Valid only for stylus touch types.
-// Zero radians indicates that the stylus is parallel to the screen surface,
-// while M_PI/2 radians indicates that it is normal to the screen surface.
-//@property(nonatomic,readonly) CGFloat altitudeAngle NS_AVAILABLE_IOS(9_1);
-
-// An index which allows you to correlate updates with the original touch.
-// Is only guaranteed non-nil if this UITouch expects or is an update.
-//@property(nonatomic,readonly) NSNumber * _Nullable estimationUpdateIndex NS_AVAILABLE_IOS(9_1);
-JS_GETTER(UITouch, ui, estimationUpdateIndex,
-  NSNumber* estimationUpdateIndex = [ui estimationUpdateIndex];
-  if (estimationUpdateIndex != nullptr) {
-    JS_SET_RETURN(JS_INT([estimationUpdateIndex integerValue]));
+NAN_METHOD(NUITouch::azimuthAngleInView) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGFloat([self azimuthAngleInView: view]));
   }
-);
-// A set of properties that has estimated values
-// Only denoting properties that are currently estimated
-//@property(nonatomic,readonly) UITouchProperties estimatedProperties NS_AVAILABLE_IOS(9_1);
-JS_GETTER(UITouch, ui, estimatedProperties,
-  JS_SET_RETURN(JS_INT([ui estimatedProperties]))
-);
-// A set of properties that expect to have incoming updates in the future.
-// If no updates are expected for an estimated property the current value is our final estimate.
-// This happens e.g. for azimuth/altitude values when entering from the edges
-//@property(nonatomic,readonly) UITouchProperties estimatedPropertiesExpectingUpdates NS_AVAILABLE_IOS(9_1);
-JS_GETTER(UITouch, ui, estimatedPropertiesExpectingUpdates,
-  JS_SET_RETURN(JS_INT([ui estimatedPropertiesExpectingUpdates]))
-);
+}
+
+NAN_METHOD(NUITouch::azimuthUnitVectorInView) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    declare_args();
+    declare_nullable_pointer(UIView, view);
+    JS_SET_RETURN(js_value_CGVector([self azimuthUnitVectorInView: view]));
+  }
+}
+
+NAN_GETTER(NUITouch::timestampGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSTimeInterval([self timestamp]));
+  }
+}
+
+NAN_GETTER(NUITouch::phaseGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UITouchPhase([self phase]));
+  }
+}
+
+NAN_GETTER(NUITouch::tapCountGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSUInteger([self tapCount]));
+  }
+}
+
+NAN_GETTER(NUITouch::typeGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UITouchType([self type]));
+  }
+}
+
+NAN_GETTER(NUITouch::majorRadiusGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_CGFloat([self majorRadius]));
+  }
+}
+
+NAN_GETTER(NUITouch::majorRadiusToleranceGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_CGFloat([self majorRadiusTolerance]));
+  }
+}
+
+#include "NUIWindow.h"
+
+NAN_GETTER(NUITouch::windowGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UIWindow([self window]));
+  }
+}
+
+NAN_GETTER(NUITouch::viewGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UIView ([self view]));
+  }
+}
+
+NAN_GETTER(NUITouch::gestureRecognizersGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_NSArray<UIGestureRecognizer*>([self gestureRecognizers]));
+  }
+}
+
+NAN_GETTER(NUITouch::forceGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_CGFloat([self force]));
+  }
+}
+
+NAN_GETTER(NUITouch::maximumPossibleForceGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_CGFloat([self maximumPossibleForce]));
+  }
+}
+
+NAN_GETTER(NUITouch::altitudeAngleGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_CGFloat([self altitudeAngle]));
+  }
+}
+
+NAN_GETTER(NUITouch::estimationUpdateIndexGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_id([self estimationUpdateIndex])); // TODO: js_value_NSNumber
+  }
+}
+
+NAN_GETTER(NUITouch::estimatedPropertiesGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UITouchProperties([self estimatedProperties]));
+  }
+}
+
+NAN_GETTER(NUITouch::estimatedPropertiesExpectingUpdatesGetter) {
+  JS_UNWRAP(UITouch, self);
+  declare_autoreleasepool {
+    JS_SET_RETURN(js_value_UITouchProperties([self estimatedPropertiesExpectingUpdates]));
+  }
+}
