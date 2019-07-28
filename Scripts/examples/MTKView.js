3138c071087b9773d56e8d64630136955680a0d4
Shawn Presser <shawnpresser@gmail.com>
2019-07-27 21:06:33 -0500

    Update MTKView example to use texturing

--- Scripts/examples/MTKView.js
+++ Scripts/examples/MTKView.js
@@ -17,9 +17,10 @@ FPSUpdate = function FPSUpdate() {
     FPSCurrent = FPSCurrentFrames;
     FPSCurrentFrames = 0;
     FPSCurrentSecond = thisSecond;
+    console.log(`FPS: ${FPSCurrent}`);
   }
   return FPSCurrent;
-}
+};
 
 FPSGet = function FPSGet() {
   return FPSCurrent;
@@ -33,7 +34,7 @@ CameraController = class CameraController {
     this._axis = new THREE.Vector3();
     this._dirty = true;
 
-    this.radius = 3;
+    this.radius = 1.5;
     this.sensitivity = 0.01;
     this.minAltitude = -Math.PI / 4.0;
     this.maxAltitude = Math.PI / 2.0;
@@ -116,6 +117,7 @@ VertexBufferIndex = {
 
 FragmentBufferIndex = {
   uniforms: 0,
+  texture: 1,
 };
 
 Uniforms = class Uniforms {
@@ -157,22 +159,84 @@ Uniforms = class Uniforms {
   }
 };
 
-async function make(nav, demoVC) {
-  device = MTLCreateSystemDefaultDevice();
-  if (!device) {
-    return;
+MTKViewExample = typeof MTKViewExample !== 'undefined' ? MTKViewExample : function MTKViewExample(...args) {
+  this.init(...args);
+};
+
+MTKViewExample.prototype.init = function(opts = {
+  device: MTLCreateSystemDefaultDevice(),
+}) {
+  if (!opts.device) {
+    throw new Error("device is null");
+  }
+  this.opts = { ...opts };
+};
+
+MTKViewExample.prototype.getDevice = function() {
+  return this.opts.device;
+};
+
+MTKRegionMake = (x, y, z, width, height, depth) => {
+  return {
+    origin: { x, y, z },
+    size: { width, height, depth },
+  }
+};
+
+MTKViewExample.prototype.makeTexture = function(image, width, height, region = MTKRegionMake(0, 0, 0, width, height, 1), device = this.getDevice()) {
+  const textureDescriptor = MTLTextureDescriptor();
+  textureDescriptor.pixelFormat = MTLPixelFormatBGRA8Unorm;
+  textureDescriptor.width = width;
+  textureDescriptor.height = height;
+  const texture = device.newTextureWithDescriptor(textureDescriptor);
+
+  this.updateTexture(texture, image, width, height, region);
+
+  return texture;
+};
+
+MTKViewExample.prototype.updateTexture = function(texture, image, width, height, region = MTKRegionMake(0, 0, 0, width, height, 1)) {
+  texture.replaceRegionMipmapLevelWithBytesBytesPerRow(region, 0, image, 4*width);
+  return texture;
+};
+
+MTKViewExample.prototype.makeDescriptor = function() {
+  const vertexDescriptor = MDLVertexDescriptor();
+  vertexDescriptor.addOrReplaceAttribute(
+    MDLVertexAttribute.alloc().initWithNameFormatOffsetBufferIndex(
+      MDLVertexAttributePosition, MDLVertexFormatFloat3, 0, 0));
+  vertexDescriptor.addOrReplaceAttribute(
+    MDLVertexAttribute.alloc().initWithNameFormatOffsetBufferIndex(
+      MDLVertexAttributeTextureCoordinate, MDLVertexFormatFloat2, 4*3, 0));
+  const mtlVertexDescriptor = MTKMetalVertexDescriptorFromModelIOWithError(vertexDescriptor);
+  mtlVertexDescriptor.layouts.objectAtIndexedSubscript(VertexBufferIndex.attributes).stride = 5*4;
+  return mtlVertexDescriptor;
+};
+
+MTKView_make = async function make(nav, demoVC) {
+  if (!MTKViewExample.instance) {
+    MTKViewExample.instance = new MTKViewExample();
   }
 
+  device = MTKViewExample.instance.getDevice();
   cam = new CameraController();
+  cam.radius = 1.5;
   proj = new ProjectionController();
   uniforms = new Uniforms();
   mvp = new THREE.Matrix4();
   uniforms.modelViewProjectionMatrix = mvp.set(...proj.projectionMatrix.elements).multiply(cam.viewMatrix);
 
   view = demoVC.view;
-  mtkView = MTKView(view.frame, device);
-  view.addSubview(mtkView);
-  mtkView.pinToSuperview();
+  const frame = {...view.frame};
+  /*
+  if (frame.width > 512) {
+    frame.width = 512;
+  }
+  if (frame.height > 512) {
+    frame.height = 512;
+  }
+  */
+  mtkView = MTKView(frame, device);
 
   mtkView.touchesBeganWithEvent = (touches, event) => {
     touches = Array.from(touches);
@@ -190,11 +254,38 @@ async function make(nav, demoVC) {
     //console.log(cam.altitude, cam.azimuth, cam.viewMatrix);
   };
 
-  mtkView.device = device;
+  //mtkView.device = device;
   mtkView.clearColor = /*MTLClearColorMake*/ UIColor(0.3, 0.3, 0.3, 1.0);
   mtkView.colorPixelFormat = MTLPixelFormatBGRA8Unorm;
   //mtkView.depthStencilPixelFormat = MTLPixelFormatDepth32Float;
   mtkView.sampleCount = 1;
+
+  mtkImageWidth = 64;
+  mtkImageHeight = 64;
+  mtkImage = new Uint8ClampedArray(4*mtkImageWidth*mtkImageHeight);
+
+  mtkImageRegen = () => {
+    const h = mtkImageHeight;
+    const w = mtkImageWidth;
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        const i = 4*(y*h + x);
+        const v = Math.random();
+        mtkImage[i+0] = 0xFF * v;
+        mtkImage[i+1] = 0xFF * v;
+        mtkImage[i+2] = 0xFF * v;
+        mtkImage[i+3] = 0xFF;
+      }
+    }
+  };
+  mtkImageRegen();
+  if (typeof mtkImageInterval !== 'undefined') {
+    clearInterval(mtkImageInterval);
+    delete mtkImageInterval;
+  };
+  mtkImageInterval = setInterval(() => { mtkImageRegen(); }, 1000 / 60);
+
+  mtkTexture = MTKViewExample.instance.makeTexture(mtkImage, mtkImageWidth, mtkImageHeight);
 
   mtkView.delegate = MTKViewDelegate({
     mtkViewDrawableSizeWillChange(mtkView, size) {
@@ -205,34 +296,29 @@ async function make(nav, demoVC) {
     },
     drawInMTKView(mtkView) {
       FPSUpdate();
-      drawable = mtkView.currentDrawable;
-      renderPassDescriptor = mtkView.currentRenderPassDescriptor;
-      if (drawable && renderPassDescriptor) {
+      const drawable = mtkView.currentDrawable;
+      if (drawable && drawable.texture && mtkView.currentRenderPassDescriptor) {
+        const region = MTKRegionMake(0, 0, 0, mtkImageWidth, mtkImageHeight, 1);
+        mtkTexture.replaceRegionMipmapLevelWithBytesBytesPerRow(region, 0, mtkImage, mtkImageWidth*4);
+        const renderPassDescriptor = mtkView.currentRenderPassDescriptor;
         uniforms.color = UIColor(Math.random(), Math.random(), Math.random(), 1.0);
         renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).texture = drawable.texture;
         renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).loadAction = MTLLoadActionClear;
         renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).clearColor = /*MTLClearColor*/ RGB(0, 104, 55, 1.0);
-        commandBuffer = commandQueue.commandBuffer();
+        const commandBuffer = commandQueue.commandBuffer();
         renderEncoder = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor);
         renderEncoder.setRenderPipelineState(pipelineState);
         renderEncoder.setVertexBytesLengthAtIndex(uniforms.buffer, uniforms.length, VertexBufferIndex.uniforms);
         renderEncoder.setFragmentBytesLengthAtIndex(uniforms.buffer, uniforms.length, FragmentBufferIndex.uniforms);
+        renderEncoder.setFragmentTextureAtIndex(mtkTexture, FragmentBufferIndex.texture);
         renderEncoder.setVertexBufferWithOffsetAtIndex(vertexBuffer, 0, 0);
-        renderEncoder.drawPrimitivesVertexStartVertexCountInstanceCount(MTLPrimitiveTypeTriangle, 0, 3, 1);
+        renderEncoder.drawPrimitivesVertexStartVertexCountInstanceCount(MTLPrimitiveTypeTriangle, 0, vertexBuffer.length, 1);
         renderEncoder.endEncoding();
         commandBuffer.presentDrawable(drawable);
         commandBuffer.commit();
       }
     },
   });
-
-  vertexData = new Float32Array([
-    0.0,  1.0, 0.0,
-    -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0,
-  ]);
-  dataSize = vertexData.length * 4;
-  vertexBuffer = device.newBufferWithBytesLengthOptions(vertexData.buffer, dataSize, 0);
 
   metalLibrary = device.newLibraryWithSourceOptionsError(`
   
@@ -244,7 +330,8 @@ enum {
 };
 
 enum {
-    fragmentBufferIndexUniforms = 0
+    fragmentBufferIndexUniforms = 0,
+    fragmentBufferIndexTexture = 1
 };
 
 struct Uniforms {
@@ -255,6 +342,14 @@ struct Uniforms {
   //float3 cameraPos;
   //float3 directionalLightInvDirection;
   //float3 lightPosition;
+};
+
+struct Vertex {
+    float3 position  [[attribute(0)]];
+    //float3 normal    [[attribute(1)]];
+    //float3 tangent   [[attribute(2)]];
+    //float2 texCoords [[attribute(3)]];
+    float2 texCoords [[attribute(1)]];
 };
 
 struct VertexOut {
@@ -283,19 +378,62 @@ fragment half4 basic_fragment(VertexOut in                     [[stage_in]],
 {
   return half4(uniforms.color);
 }
+
+vertex VertexOut vertex_main(Vertex in [[stage_in]],
+                             constant Uniforms &uniforms [[buffer(vertexBufferIndexUniforms)]])
+{
+    VertexOut out;
+    out.position = uniforms.modelViewProjectionMatrix * float4(in.position, 1.0);
+    out.texCoords = in.texCoords;
+    //out.normal = uniforms.normalMatrix * in.normal;
+    //out.tangent = uniforms.normalMatrix * in.tangent;
+    //out.bitangent = uniforms.normalMatrix * cross(in.normal, in.tangent);
+    //out.worldPos = (uniforms.modelMatrix * float4(in.position, 1.0)).xyz;
+    return out;
+}
+
+fragment half4 fragment_main(VertexOut in                     [[stage_in]],
+                             constant Uniforms &uniforms      [[buffer(fragmentBufferIndexUniforms)]],
+                             texture2d<half> colorTexture     [[texture(fragmentBufferIndexTexture)]])
+{
+  constexpr sampler textureSampler (mag_filter::linear,
+                                    min_filter::linear);
+
+  const half4 diffuseColor = colorTexture.sample(textureSampler, in.texCoords);
+  const half4 lightColor = 0.5 + half4(uniforms.color);
+  const half4 resultColor = diffuseColor * lightColor;
+  return resultColor;
+}
 `, MTLCompileOptions());
 
-  fragmentProgram = metalLibrary.newFunctionWithName("basic_fragment");
-  vertexProgram = metalLibrary.newFunctionWithName("basic_vertex");
+  //fragmentProgram = metalLibrary.newFunctionWithName("basic_fragment");
+  //vertexProgram = metalLibrary.newFunctionWithName("basic_vertex");
+  fragmentProgram = metalLibrary.newFunctionWithName("fragment_main");
+  vertexProgram = metalLibrary.newFunctionWithName("vertex_main");
 
   pipelineStateDescriptor = MTLRenderPipelineDescriptor();
   pipelineStateDescriptor.vertexFunction = vertexProgram;
   pipelineStateDescriptor.fragmentFunction = fragmentProgram;
   pipelineStateDescriptor.colorAttachments.objectAtIndexedSubscript(0).pixelFormat = MTLPixelFormatBGRA8Unorm;
+  vertexDescriptor = MTKViewExample.instance.makeDescriptor();
+  pipelineStateDescriptor.vertexDescriptor = vertexDescriptor;
 
   pipelineState = device.newRenderPipelineStateWithDescriptorError(pipelineStateDescriptor);
 
-  commandQueue = device.newCommandQueue();
-}
+  vertexData = new Float32Array([
+    -1.0, +1.0, 0.0,   -0.0, +1.0,
+    -1.0, -1.0, 0.0,   -0.0, -0.0,
+    +1.0, -1.0, 0.0,   +1.0, -0.0,
 
-module.exports = make;
+    -1.0, +1.0, 0.0,   -0.0, +1.0,
+    +1.0, -1.0, 0.0,   +1.0, -0.0,
+    +1.0, +1.0, 0.0,   +1.0, +1.0,
+  ]);
+  vertexBuffer = device.newBufferWithBytesLengthOptions(vertexData.buffer, vertexData.length * 4, 0);
+
+  commandQueue = device.newCommandQueue();
+  view.addSubview(mtkView);
+  mtkView.pinToSuperview();
+};
+
+module.exports = (...args) => MTKView_make(...args);
